a2e3020131965aa7863f4e42cd44301280ff691e
Alexandra <carpenamarie@itwm.fraunhofer.de>
2020-10-25 19:28:34 +0100

    Tests: python: datasets: use a GPI test to be able to import `tarantella`

--- test/python/conftest.py
+++ test/python/conftest.py
@@ -1,5 +1,3 @@
-import tarantella
-
 import pytest
 import logging
 import os
@@ -8,7 +6,9 @@ import os
 def tarantella_framework():
   os.environ['TF_CUDNN_DETERMINISTIC']='1'
 
+  import tarantella
   tarantella.init()
+
   logging.getLogger().info("init tarantella")
   yield tarantella  # provide the fixture value
   logging.getLogger().info("teardown tarantella")
