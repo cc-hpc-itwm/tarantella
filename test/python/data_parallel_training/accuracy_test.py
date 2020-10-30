220b8b4d8720883da83eef352baa9bbda5582ceb
Peter Labus <peter.labus@itwm.fraunhofer.de>
2020-10-30 15:22:14 +0100

    test: pytest: Remove AlexNet (takes too long)

--- test/python/data_parallel_training/accuracy_test.py
+++ test/python/data_parallel_training/accuracy_test.py
@@ -16,7 +16,6 @@ import pytest
 @pytest.fixture(scope="class", params=[mnist.fc_model_generator,
                                        mnist.lenet5_model_generator,
                                        mnist.sequential_model_generator,
-                                       mnist.alexnet_model_generator
                                       ])
 def model_runners(request):
   tf.random.set_seed(42)
