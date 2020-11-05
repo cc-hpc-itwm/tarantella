8497bc444c18472e9bb612a47326cf7d690f269f
Alexandra <carpenamarie@itwm.fraunhofer.de>
2020-11-05 17:52:43 +0100

    Tarantella: datasets: process each dataset transformation in separate functions
    * update parameters for all transformations that can be applied to an
    existing `tf.data.Dataset`
    * fix logging

--- src/tarantella/datasets/dataset_helpers.py
+++ src/tarantella/datasets/dataset_helpers.py
@@ -4,77 +4,139 @@ from tensorflow.python.framework import ops
 from tensorflow.python.framework import dtypes
 from tensorflow.python.ops import gen_dataset_ops
 
-import logging
+from tarantella import logger
+import tarantella.datasets.ops as tnt_ops
 
-class TarantellaParallelMapDataset(ds.UnaryDataset):
-  """A `Dataset` that maps a function over the elements in its input."""
-  def __init__(self,
-               input_dataset,
-               map_func,
-               num_parallel_calls,
-               use_inter_op_parallelism=True,
-               preserve_cardinality=False,
-               use_legacy_function=False):
-    self._input_dataset = input_dataset
-    self._use_inter_op_parallelism = use_inter_op_parallelism
-    self._num_parallel_calls = ops.convert_to_tensor(
-        num_parallel_calls, dtype=dtypes.int32, name="num_parallel_calls")
-    self._preserve_cardinality = preserve_cardinality
-    self._map_func = map_func # StructuredFunctionWrapper
+def _get_transformation_info_batch(dataset):
+  kwargs = {"batch_size": dataset._batch_size,
+            "drop_remainder": dataset._drop_remainder}
+  return (ds.BatchDataset, kwargs)
 
-    variant_tensor = gen_dataset_ops.map_dataset(
-        input_dataset._variant_tensor,  # pylint: disable=protected-access
-        self._map_func.function.captured_inputs,
-        f=self._map_func.function,
-        use_inter_op_parallelism=self._use_inter_op_parallelism,
-        preserve_cardinality=self._preserve_cardinality,
-        **self._flat_structure)
-    super(TarantellaParallelMapDataset, self).__init__(
-      input_dataset, variant_tensor)
+def _get_transformation_info_cache(dataset):
+  kwargs = {"filename": dataset._filename}
+  return (ds.CacheDataset, kwargs)
 
-  def _functions(self):
-    return [self._map_func]
+def _get_transformation_info_filter(dataset):
+  kwargs = {"predicate": dataset._predicate}
+  return (tnt_ops.TntFilterDataset, kwargs)
 
-  @property
-  def element_spec(self):
-    return self._map_func.output_structure
+def _get_transformation_info_flatmap(dataset):
+  kwargs = {"map_func": dataset._map_func}
+  return (tnt_ops.TntFlatMapDataset, kwargs)
 
-  def _transformation_name(self):
-    return "Dataset.map()"
+def _get_transformation_info_interleave(dataset):
+  kwargs = {"map_func": dataset._map_func,
+            "cycle_length": dataset._cycle_length,
+            "block_length": dataset._block_length}
+  return (tnt_ops.TntInterleaveDataset, kwargs)
 
+def _get_transformation_info_map(dataset):
+  kwargs = {"map_func": dataset._map_func,
+            "use_inter_op_parallelism": dataset._use_inter_op_parallelism,
+            "preserve_cardinality": dataset._preserve_cardinality}
+  return (tnt_ops.TntMapDataset, kwargs)
 
-class TarantellaMapDataset(ds.UnaryDataset):
-  """A `Dataset` that maps a function over the elements in its input."""
-  def __init__(self,
-               input_dataset,
-               map_func,
-               use_inter_op_parallelism=True,
-               preserve_cardinality=False,
-               use_legacy_function=False):
-    """See `Dataset.map()` for details."""
-    self._input_dataset = input_dataset
-    self._use_inter_op_parallelism = use_inter_op_parallelism
-    self._preserve_cardinality = preserve_cardinality
-    self._map_func = map_func # StructuredFunctionWrapper
+def _get_transformation_info_paddedbatch(dataset):
+  kwargs = {"batch_size": dataset._batch_size,
+            "padded_shapes": dataset._padded_shapes,
+            "padding_values": dataset._padding_values,
+            "drop_remainder": dataset._drop_remainder}
+  return (tnt_ops.TntPaddedBatchDataset, kwargs)
 
-    variant_tensor = gen_dataset_ops.map_dataset(
-        input_dataset._variant_tensor,  # pylint: disable=protected-access
-        self._map_func.function.captured_inputs,
-        f=self._map_func.function,
-        use_inter_op_parallelism=self._use_inter_op_parallelism,
-        preserve_cardinality=self._preserve_cardinality,
-        **self._flat_structure)
-    super(TarantellaMapDataset, self).__init__(input_dataset, variant_tensor)
+def _get_transformation_info_parallelinterleave(dataset):
+  kwargs = {"map_func": dataset._map_func,
+            "cycle_length": dataset._cycle_length,
+            "block_length": dataset._block_length,
+            "num_parallel_calls": dataset._num_parallel_calls,
+            "buffer_output_elements": dataset._buffer_output_elements,
+            "prefetch_input_elements": dataset._prefetch_input_elements,
+            "deterministic": dataset._deterministic}
+  return (tnt_ops.TntParallelInterleaveDataset, kwargs)
 
-  def _functions(self):
-    return [self._map_func]
+def _get_transformation_info_parallelmap(dataset):
+  kwargs = {"map_func": dataset._map_func,
+            "use_inter_op_parallelism": dataset._use_inter_op_parallelism,
+            "num_parallel_calls": dataset._num_parallel_calls,
+            "preserve_cardinality": dataset._preserve_cardinality,
+            "deterministic": dataset._deterministic}
+  return (tnt_ops.TntParallelMapDataset, kwargs)
 
-  @property
-  def element_spec(self):
-    return self._map_func.output_structure
+def _get_transformation_info_prefetch(dataset):
+  buffer_size = dataset._buffer_size
+  # TF2.2: https://github.com/tensorflow/tensorflow/blob/v2.2.0/tensorflow/python/data/ops/dataset_ops.py#L4255
+  if buffer_size == -1:
+    buffer_size = None
+  kwargs = {"buffer_size" : dataset._buffer_size}
+  return (ds.PrefetchDataset, kwargs)
 
-  def _transformation_name(self):
-    return "Dataset.map()"
+def _get_transformation_info_repeat(dataset):
+  count = dataset._count
+  if count == -1:
+    count = None
+  kwargs = {"count": count}
+  return (ds.RepeatDataset, kwargs)
+
+def _get_transformation_info_shard(dataset):
+  kwargs =  {"num_shards": dataset._num_shards,
+             "index": dataset._index}
+  return (ds.ShardDataset, kwargs)
+
+def _get_transformation_info_shuffle(dataset):
+  # TF 2.0 - 2.2
+  # ShuffleDataset does not save the given seed
+  # instead it has two seed properties defined as
+  # `self._seed, self._seed2 = random_seed.get_seed(seed)`
+  # with `get_seed` defined in `tensorflow/python/framework/random_seed.py` [TF2.2]
+  if dataset._seed2 == 0:
+    # there was no seed specified by the user
+    seed = None
+  else:
+    seed = dataset._seed2
+  kwargs = {"buffer_size": dataset._buffer_size,
+            "seed": seed,
+            "reshuffle_each_iteration": dataset._reshuffle_each_iteration}
+  return (ds.ShuffleDataset, kwargs)
+
+def _get_transformation_info_skip(dataset):
+  kwargs = {"count": dataset._count}
+  return (ds.SkipDataset, kwargs)
+
+def _get_transformation_info_take(dataset):
+  kwargs = {"count": dataset._count}
+  return (ds.TakeDataset, kwargs)
+
+def _get_transformation_info_unbatch(dataset):
+  kwargs = {}
+  return (ds._UnbatchDataset, kwargs)
+
+def _get_transformation_info_window(dataset):
+  kwargs = {"size": dataset._size,
+            "shift": dataset._shift,
+            "stride": dataset._stride,
+            "drop_remainder": dataset._drop_remainder}
+  return (ds.WindowDataset, kwargs)
+
+def _get_transformation_info_withoptions(dataset):
+  kwargs = {"options": dataset._options}
+  return (ds._OptionsDataset, kwargs)
+
+_transformations = {ds.BatchDataset : _get_transformation_info_batch,
+                    ds.CacheDataset : _get_transformation_info_cache,
+                    ds.FilterDataset : _get_transformation_info_filter,
+                    ds.FlatMapDataset : _get_transformation_info_flatmap,
+                    ds.InterleaveDataset : _get_transformation_info_interleave,
+                    ds.MapDataset : _get_transformation_info_map,
+                    ds.PaddedBatchDataset : _get_transformation_info_paddedbatch,
+                    ds.PrefetchDataset : _get_transformation_info_prefetch,
+                    ds.RepeatDataset : _get_transformation_info_repeat,
+                    ds.ShardDataset : _get_transformation_info_shard,
+                    ds.ShuffleDataset : _get_transformation_info_shuffle,
+                    ds.SkipDataset : _get_transformation_info_skip,
+                    ds.TakeDataset : _get_transformation_info_take,
+                    ds._UnbatchDataset : _get_transformation_info_unbatch,
+                    ds.WindowDataset : _get_transformation_info_window,
+                    ds._OptionsDataset : _get_transformation_info_withoptions,  
+                    }
 
 def gen_dataset_transformations(dataset):
   """Generate the list of transformations that has been applied to a dataset
@@ -82,75 +144,10 @@ def gen_dataset_transformations(dataset):
   """
   stack = []
   while (hasattr(dataset, '_input_dataset')):
-    if isinstance(dataset, ds.BatchDataset):
-        kwargs = {"batch_size": dataset._batch_size,
-                  "drop_remainder": dataset._drop_remainder}    
-    elif isinstance(dataset, ds.PaddedBatchDataset):
-        kwargs = {"batch_size": dataset._batch_size,
-                  "drop_remainder": dataset._drop_remainder,
-                  "padded_shapes": dataset._padded_shapes,
-                  "padding_values": dataset._padding_values}        
-    elif isinstance(dataset, ds.PrefetchDataset):
-        kwargs = {"buffer_size": dataset._buffer_size}            
-    elif isinstance(dataset, ds.RepeatDataset)  \
-      or isinstance(dataset, ds.SkipDataset)    \
-      or isinstance(dataset, ds.TakeDataset):
-        kwargs = {"count": dataset._count}            
-    elif isinstance(dataset, ds.ShuffleDataset):
-        # ShuffleDataset does not save the given seed
-        # instead it has two seed properties defined as
-        # `self._seed, self._seed2 = random_seed.get_seed(seed)`
-        # with `get_seed` defined in `tensorflow/python/framework/random_seed.py` [TF2.2]
-        if dataset._seed2 == 0:
-          # there was no seed specified by the user
-          seed = None
-        else:
-          seed = dataset._seed2
-        kwargs = {"buffer_size": dataset._buffer_size,
-                  "seed" : seed,
-                  "reshuffle_each_iteration" : dataset._reshuffle_each_iteration}
-    elif isinstance(dataset, ds.CacheDataset):
-        kwargs = {"filename": dataset._filename} 
-    elif isinstance(dataset, ds.FilterDataset):
-        kwargs = {"predicate": dataset._predicate}
-    elif isinstance(dataset, ds.FlatMapDataset):
-        kwargs = {"map_func": dataset._map_func}
-    elif isinstance(dataset, ds.MapDataset):
-        kwargs = {"map_func": dataset._map_func,
-                  "preserve_cardinality": dataset._preserve_cardinality}
-    elif isinstance(dataset, ds.ParallelMapDataset):
-        kwargs = {"map_func": dataset._map_func,
-                  "preserve_cardinality": dataset._preserve_cardinality,
-                  "num_parallel_calls": dataset._num_parallel_calls}          
-    elif isinstance(dataset, ds.InterleaveDataset):
-        kwargs = {"map_func": dataset._map_func,
-                  "cycle_length": dataset._cycle_length,
-                  "block_length": dataset._block_length}
-    elif isinstance(dataset, ds.ParallelInterleaveDataset):
-        kwargs = {"map_func": dataset._map_func,
-                  "cycle_length": dataset._cycle_length,
-                  "block_length": dataset._block_length,
-                  "num_parallel_calls": dataset._num_parallel_calls}
-    elif isinstance(dataset, ds.ShardDataset):
-        kwargs = {"num_shards": dataset._num_shards,
-                  "index": dataset._index} 
-    elif isinstance(dataset, ds.WindowDataset):
-        kwargs = {"size": dataset._size,
-                  "shift": dataset._shift,
-                  "stride": dataset._stride,
-                  "drop_remainder": dataset._drop_remainder}
-    elif isinstance(dataset, ds._OptionsDataset):
-        kwargs = {"options": dataset._options}
-    else:
-        # TODO raise error if unknown dataset
-        kwargs = {}
-    
-    if isinstance(dataset, ds.ParallelMapDataset):
-      stack.append((TarantellaParallelMapDataset, kwargs))
-    elif isinstance(dataset, ds.MapDataset):
-      stack.append((TarantellaMapDataset, kwargs))
-    else:
-      stack.append((type(dataset), kwargs))
+    for transformation in _transformations:
+      if isinstance(dataset, transformation):
+        stack.append(_transformations[transformation](dataset))
+        break
     dataset = dataset._input_dataset
   return (dataset, list(reversed(stack)))
 
@@ -166,17 +163,17 @@ def get_num_samples(dataset):
   cardinality = tf.data.experimental.cardinality(dataset)
 
   if cardinality == tf.data.experimental.INFINITE_CARDINALITY:
-    logging.getLogger().debug("Infinite dataset detected.")
+    logger.debug("Infinite dataset detected.")
     return tf.data.experimental.INFINITE_CARDINALITY
 
   if cardinality != tf.data.experimental.UNKNOWN_CARDINALITY:
-    logging.getLogger().debug("Dataset size is %d" % (cardinality.numpy()))
+    logger.debug("Dataset size is %d" % (cardinality.numpy()))
     return cardinality.numpy()
 
-  logging.getLogger().debug("Unknown dataset size. Counting samples...")
+  logger.debug("Unknown dataset size. Counting samples...")
   dataset_size = 0
   for d in dataset:
     dataset_size += 1
-  logging.getLogger().debug("Dataset size is %d" % (dataset_size))
+  logger.debug("Dataset size is %d" % (dataset_size))
   return dataset_size
 
