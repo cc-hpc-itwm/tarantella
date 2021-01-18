e92abf2e7bb3847f1a7c8a25332261974f31caab
Alexandra <carpenamarie@itwm.fraunhofer.de>
2021-01-18 17:26:01 +0100

    Test: Pytest: remove model fixtures from optimizer-accuracy tests
    * fixtures would be useful if they prevent the creation of new `SynchCommunicators`,
    but it is not the case when re-compiling models with a different optimizer
    * separate SGD with momentum tests to avoid creating too many segments in tests

--- test/python/data_parallel_training/optimizerSGD_momentum_mnist_test.py
+++ test/python/data_parallel_training/optimizerSGD_momentum_mnist_test.py
@@ -7,33 +7,29 @@ from tensorflow import keras
 
 import pytest
 
-class TestsDataParallelOptimizers:
-  def test_initialization(self, tarantella_framework):
-    assert tarantella_framework
-
+class TestsSGDMomentumOptimizer:
   @pytest.mark.parametrize("keras_model", [mnist.lenet5_model_generator,
                                            mnist.sequential_model_generator])
-  @pytest.mark.parametrize("optimizer", [keras.optimizers.Adadelta,
-                                         keras.optimizers.Adagrad,
-                                         keras.optimizers.Adam,
-                                         keras.optimizers.Adamax,
-                                         keras.optimizers.Nadam,
-                                         keras.optimizers.RMSprop,
-                                        ])
+  @pytest.mark.parametrize("lr", [0.01])
+  @pytest.mark.parametrize("nesterov", [False, True])
+  @pytest.mark.parametrize("momentum", [0.9])
   @pytest.mark.parametrize("micro_batch_size", [64])
   @pytest.mark.parametrize("nbatches", [230])
-  def test_compare_accuracy_optimizers(self, tarantella_framework, keras_model,
-                                      optimizer, micro_batch_size, nbatches):
+  @pytest.mark.parametrize("number_epochs", [8])
+  def test_compare_sgd_momentum(self, tarantella_framework, keras_model,
+                                lr, nesterov, momentum, micro_batch_size, nbatches,
+                                number_epochs):
     batch_size = micro_batch_size * tarantella_framework.get_size()
     nsamples = nbatches * batch_size
-    (number_epochs, lr) = mnist.get_hyperparams(optimizer)
     (train_dataset, test_dataset) = util.load_dataset(mnist.load_mnist_dataset,
                                                       train_size = nsamples,
                                                       train_batch_size = batch_size,
                                                       test_size = 10000,
                                                       test_batch_size = batch_size)
     model = tarantella.Model(keras_model())
-    model.compile(optimizer(learning_rate=lr),
+    model.compile(keras.optimizers.SGD(learning_rate=lr,
+                                       momentum=momentum,
+                                       nesterov=nesterov),
                   loss = keras.losses.SparseCategoricalCrossentropy(),
                   metrics = [keras.metrics.SparseCategoricalAccuracy()])
     model.fit(train_dataset,
