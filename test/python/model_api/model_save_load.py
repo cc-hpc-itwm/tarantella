76892412fa650f93df3b59ef87110bb4bc403e87
Pengqiu Li <pengqiu.li@itwm.fraunhofer.de>
2021-02-16 13:10:54 +0100

    tests: pytest: model_api: use absolute path of the test file and clean it up at the end of each test

--- test/python/model_api/model_save_load.py
+++ test/python/model_api/model_save_load.py
@@ -9,6 +9,8 @@ import copy
 import numpy as np
 import os
 import pytest
+
+import shutil
 import tempfile
 
 # saving/loading the whole model applies to keras models, subclassed models, or Sequential
@@ -20,27 +22,35 @@ def model(request):
   yield request.param()
 
 class TestsModelLoadSave:
-
+  @pytest.mark.tfversion('2.2')
   def test_save_before_compile(self, tarantella_framework, model):
-    with tempfile.TemporaryDirectory() as tmpdirname:
-      file_path = os.path.join(tmpdirname, "model.save")
+    ##tempfile name is not reproducible so we shouldn't use it here. Otherwise every rank would get a different folder.
+    ##other rank have different cwd from rank 0, so I use the abspath of this file,which shoulbe be in the shared file as well
+    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"test_save_before_compile")
+    
+    tnt_model = tnt.Model(model)
 
-      tnt_model = tnt.Model(model)
-
-      # save Tarantella model
-      assert not os.path.exists(file_path)
-      tnt_model.save(file_path)
-      assert os.path.exists(file_path)
-
+    tnt_model.save(file_path)
+    
+    assert os.path.exists(file_path)
       # load file into a new Tarantella model
-      loaded_model = tnt.models.load_model(file_path,compile=True)
-      assert isinstance(loaded_model, tnt.Model)
+    loaded_model = tnt.models.load_model(file_path,compile=True)
+    assert isinstance(loaded_model, tnt.Model)
 
-      assert util.is_model_configuration_identical(loaded_model, tnt_model)
+    assert util.is_model_configuration_identical(loaded_model, tnt_model)
+    tnt_model.barrier.synchronize()
+    
+    ##clean the path
+    if tnt.get_rank() == 0:
+      shutil.rmtree(file_path, ignore_errors=True)
+    
+    tnt_model.barrier.synchronize()
+    
 
   @pytest.mark.parametrize("load_compiled_model", [True, False])
   @pytest.mark.parametrize("micro_batch_size", [64])
   @pytest.mark.parametrize("nbatches", [12])
+  @pytest.mark.tfversion('2.2')
   def test_accuracy_loaded_model(self, tarantella_framework, model,
                                  load_compiled_model, micro_batch_size, nbatches):
     batch_size = micro_batch_size * tarantella_framework.get_size()
@@ -61,25 +71,31 @@ class TestsModelLoadSave:
                   verbose = 0)
     result = tnt_model.evaluate(test_dataset,
                                 verbose = 0)
-
-    with tempfile.TemporaryDirectory() as tmpdirname:
-      file_path = os.path.join(tmpdirname, "model.save")
-
+    
+    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"test_accuracy_loaded_model")
+    
       # save trained model
-      tnt_model.save(file_path)
+    tnt_model.save(file_path)
 
       # load into a new tnt.Model
-      loaded_model = tnt.models.load_model(file_path, compile = load_compiled_model)
-      assert isinstance(loaded_model, tnt.Model)
-      assert util.is_model_configuration_identical(loaded_model, tnt_model)
+    loaded_model = tnt.models.load_model(file_path, compile = load_compiled_model)
+    assert isinstance(loaded_model, tnt.Model)
+    assert util.is_model_configuration_identical(loaded_model, tnt_model)
 
-      if not load_compiled_model:
-        loaded_model.compile(keras.optimizers.SGD(learning_rate = 0.01, momentum = 0.9),
+    if not load_compiled_model:
+      loaded_model.compile(keras.optimizers.SGD(learning_rate = 0.01, momentum = 0.9),
                              loss = keras.losses.SparseCategoricalCrossentropy(),
                              metrics = [keras.metrics.SparseCategoricalAccuracy()])
 
       # the loaded model should provide the same accuracy
-      result_loaded = loaded_model.evaluate(test_dataset,
+    result_loaded = loaded_model.evaluate(test_dataset,
                                             verbose = 0)
-      assert np.isclose(result[0], result_loaded[0], atol=1e-6)
+    assert np.isclose(result[0], result_loaded[0], atol=1e-6)
+    
+    tnt_model.barrier.synchronize()
+    
+    if tnt.get_rank() == 0:
+      shutil.rmtree(file_path, ignore_errors=True)
+    
+    tnt_model.barrier.synchronize()
 
