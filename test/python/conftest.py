b2b62d0b12a5bf60234237059a51bed03f9e12dc
Alexandra <carpenamarie@itwm.fraunhofer.de>
2021-03-08 19:40:58 +0100

    pytest: remove call to `tarantella.init()`

--- test/python/conftest.py
+++ test/python/conftest.py
@@ -9,20 +9,16 @@ def tarantella_framework():
   os.environ['TF_CUDNN_DETERMINISTIC']='1'
 
   import tarantella
-  tarantella.init()
 
   logging.getLogger().info("init tarantella")
   yield tarantella  # provide the fixture value
   logging.getLogger().info("teardown tarantella")
-
-
 
 def pytest_configure(config):
     # register an additional marker
     config.addinivalue_line(
         "markers", "tfversion(version): test to run only on specific tf versions"
     )
-
 
 def pytest_runtest_setup(item):
     supported_versions = [mark.args[0] for mark in item.iter_markers(name="tfversion")]
@@ -33,4 +29,3 @@ def pytest_runtest_setup(item):
           supportedv = v
       if not supportedv:
         pytest.skip("Test does not support TF{}".format(tf.__version__))
-
