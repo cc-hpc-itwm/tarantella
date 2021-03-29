1a30596ca85783ee49c80a5404099ba4db8e78d4
Alexandra <carpenamarie@itwm.fraunhofer.de>
2021-03-29 18:41:01 +0200

    tarantella: pipelining: Microbatched model: add support for microbatched `losses` and `metrics`

--- src/tarantella/strategy/pipelining/microbatched_model_builder.py
+++ src/tarantella/strategy/pipelining/microbatched_model_builder.py
@@ -1,9 +1,20 @@
 import tarantella.keras.layers as tnt_layers
+import tarantella.keras.losses as tnt_losses
+import tarantella.keras.metrics as tnt_metrics
+
 import tarantella.strategy.pipelining.model_builder as model_builder
 import tarantella.strategy.pipelining.partition_info as pinfo
 import tarantella.strategy.pipelining.pipeline_microbatched_dataset as dataset_utils
 
 import tensorflow.keras as keras
+
+import enum
+
+
+class ObjectiveType(enum.Enum):
+  loss = 'loss'
+  metric = 'metric'
+  loss_weight = 'loss_weight'
 
 class MicrobatchedModelBuilder(model_builder.ModelBuilder):
   def __init__(self, partition_info, shared_model, 
@@ -60,6 +71,15 @@ class MicrobatchedModelBuilder(model_builder.ModelBuilder):
     return keras.Model(inputs=inputs, outputs=outputs,
                        name=f"p_{self.partition_info.pid}_{self.shared_model.name}_microbatched")
 
+  def get_losses(self, losses_by_output_id):
+    real_losses = self.build_real_losses(losses_by_output_id)
+    edge_losses = self.build_edge_losses()
+    return {**real_losses, **edge_losses}
+
+  def get_metrics(self, metrics_by_output_id):
+    real_metrics = self.build_real_metrics(metrics_by_output_id)
+    edge_metrics = self.build_edge_metrics()
+    return {**real_metrics, **edge_metrics}
 
   def slice_microbatched_outputs(self, outputs_by_mbatch):
     real_outputs_by_mbatch = {}
@@ -122,3 +142,48 @@ class MicrobatchedModelBuilder(model_builder.ModelBuilder):
                                                          layer_id = layer_id,
                                                          micro_batch_id = mbatch_id)
     return tnt_layers.IdentityLayer(name=name)(layer)
+
+  def build_microbatched_inputs_list(self, real_inputs_by_mbatch, edge_inputs_by_mbatch,
+                                     recv_tags_by_mbatch, send_tags_by_mbatch, seq_input):
+    # assemble list of final inputs for the microbatched model
+    inputs = []
+    for mbatch_id in range(self.number_micro_batches):
+      inputs += self.merge_inputs(real_inputs = real_inputs_by_mbatch[mbatch_id],
+                                  edge_inputs = edge_inputs_by_mbatch[mbatch_id],
+                                  recv_tags = recv_tags_by_mbatch[mbatch_id],
+                                  send_tags = send_tags_by_mbatch[mbatch_id])
+    inputs += seq_input
+    return inputs
+
+  def build_objectives(self, endpoint_type, objective_type, losses_or_metrics = None):
+    microbatched_objectives = dict()
+    output_infos = self.partition_info.get_infos(endpoint_type)
+
+    for index, out_info in enumerate(output_infos):
+      for mbatch_id in range(self.number_micro_batches):
+        output_name = dataset_utils.create_name_micro_batched_layer(self.partition_info.pid,
+                                                                    element_type = endpoint_type,
+                                                                    layer_id = index,
+                                                                    micro_batch_id = mbatch_id)
+        if endpoint_type == pinfo.EndpointType.out:
+          microbatched_objectives[output_name] = losses_or_metrics[out_info.endpoint_id]
+        elif endpoint_type == pinfo.EndpointType.out_edge:
+          if objective_type == ObjectiveType.loss:
+            microbatched_objectives[output_name] = tnt_losses.ZeroLoss()
+          elif objective_type == ObjectiveType.metric:
+            microbatched_objectives[output_name] = tnt_metrics.ZeroMetric()
+
+    return microbatched_objectives
+
+  def build_real_losses(self, losses):
+    return self.build_objectives(pinfo.EndpointType.out, ObjectiveType.loss, losses)
+
+  def build_edge_losses(self):
+    return self.build_objectives(pinfo.EndpointType.out_edge, ObjectiveType.loss)
+
+  def build_real_metrics(self, metrics):
+    return self.build_objectives(pinfo.EndpointType.out, ObjectiveType.metric, metrics)
+
+  def build_edge_metrics(self):
+    return self.build_objectives(pinfo.EndpointType.out_edge, ObjectiveType.metric)
+
