4789055b150db633e35695dcbbf2c767f875e549
Peter Labus <peter.labus@itwm.fraunhofer.de>
2021-03-30 15:53:47 +0200

    tarantella: pipelining: Split between real and edge in microbatched datasets

--- src/tarantella/strategy/pipelining/pipeline_microbatched_dataset.py
+++ src/tarantella/strategy/pipelining/pipeline_microbatched_dataset.py
@@ -3,18 +3,9 @@ import tarantella.strategy.pipelining.partition_info as pinfo
 import argparse
 import re
 
+import numpy as np
 import tensorflow as tf
 from enum import Enum
-
-class ModelInoutTypes(Enum):
-    input_real = 'i'
-    output_real = 'o'
-    input_edge = 'i_edge'
-    output_edge = 'o_edge'
-    start_seq = 'start_seq'
-    end_seq = 'end_seq'
-    recv_tag = 'r_tag'
-    send_tag = 's_tag'
 
 def create_name_partition(partition_id):
   return 'p_%s' % (str(partition_id))
@@ -47,110 +38,184 @@ def create_names_micro_batched_model(partition_id, num_core_elements, element_ty
   return names
 
 # TODO: move to model generator and make sure the order matches with input construction
-def create_micro_batched_dataset(samples, labels, recv_connection_ids, send_connection_ids,
-                                 partition_id, num_micro_batches, micro_batch_size):
-  assert isinstance(samples, list) and len(samples) > 0
-  assert isinstance(labels, list) and len(labels) > 0
-  num_inputs = len(samples)
-  num_outputs = len(labels)
-  num_recvs = len(recv_connection_ids)
-  num_sends = len(send_connection_ids)
+def create_micro_batched_dataset(samples, # list of real inputs on the partition
+                                 labels, # list of real labels (outputs) on the partition 
+                                 partition_info, # contains list of EndpointInfo's for real/edge inputs/outputs (in same order as above)
+                                 num_micro_batches, # number of "replicas"
+                                 micro_batch_size):
+  assert isinstance(samples, list)
+  assert isinstance(labels, list)
 
-  input_names = create_names_micro_batched_model(partition_id=partition_id,
-                                                 num_core_elements=num_inputs,
-                                                 element_type=pinfo.EndpointType.inp,
-                                                 num_micro_batches=num_micro_batches)
-  recv_tag_names = create_names_micro_batched_model(partition_id=partition_id,
-                                                    num_core_elements=num_recvs,
-                                                    element_type=pinfo.EndpointType.recv_tag,
-                                                    num_micro_batches=num_micro_batches)
-  send_tag_names = create_names_micro_batched_model(partition_id=partition_id,
-                                                    num_core_elements=num_sends,
-                                                    element_type=pinfo.EndpointType.send_tag,
-                                                    num_micro_batches=num_micro_batches)
-  start_seq_name = create_name_micro_batched_layer(partition_id=partition_id,
-                                                   element_type=pinfo.EndpointType.seq_input)
-  output_names = create_names_micro_batched_model(partition_id=partition_id,
-                                                  num_core_elements=num_outputs,
-                                                  element_type=pinfo.EndpointType.out,
-                                                  num_micro_batches=num_micro_batches)
-  end_seq_name = create_name_micro_batched_layer(partition_id=partition_id,
-                                                 element_type=pinfo.EndpointType.seq_output)
+  num_real_inputs = len(partition_info.get_real_ids(pinfo.EndpointDirection.inp))
+  num_real_outputs = len(partition_info.get_real_ids(pinfo.EndpointDirection.out))
+  num_edge_inputs = len(partition_info.get_edge_ids(pinfo.EndpointDirection.inp))
+  num_edge_outputs = len(partition_info.get_edge_ids(pinfo.EndpointDirection.out))
 
-  # create micro-batched inputs/outputs from original samples/labels and
-  # an additional placeholder dataset each, for sequential micro-batch execution
-  input_datasets = list()
-  for i in range(num_inputs):
-    for m in range(num_micro_batches):
-      input_datasets.append(samples[i].batch(micro_batch_size).shard(num_micro_batches, m))
+  assert num_real_inputs == len(samples)
+  assert num_real_outputs == len(labels)
 
-  recv_tag_datasets = list()
-  for connection_id in recv_connection_ids:
-    for m in range(num_micro_batches):
-      recv_tag_datasets.append(tf.data.Dataset.from_tensors(tf.constant([m, connection_id], dtype=tf.int32)).batch(1).repeat())
-
-  send_tag_datasets = list()
-  for connection_id in send_connection_ids:
-    for m in range(num_micro_batches):
-      send_tag_datasets.append(tf.data.Dataset.from_tensors(tf.constant([m, connection_id], dtype=tf.int32)).batch(1).repeat())
-
-  start_seq_dataset = [tf.data.Dataset.from_tensors(tf.constant(0.0, dtype=tf.float32)).batch(1).repeat()]
-
-  output_datasets = list()
-  for o in range(num_outputs):
-    for m in range(num_micro_batches):
-      output_datasets.append(labels[o].batch(micro_batch_size).shard(num_micro_batches, m))
-
-  end_seq_dataset = [tf.data.Dataset.from_tensors(tf.constant(0.0, dtype=tf.float32)).batch(1).repeat()]
-
-  # create generator function that implements an iterator over pairs of
-  # input/output dict's, which map from the input,recv_tag,send_tag/output layer name
-  # to an input,recv_tag,send_tag/output sample
+  real_input_datasets = build_microbatched_datasets_real(
+    pinfo.EndpointType.inp, samples, num_micro_batches, micro_batch_size)
+  edge_input_datasets = build_microbatched_datasets_edge(
+    pinfo.EndpointType.inp_edge, partition_info, num_micro_batches, micro_batch_size)
+  recv_tag_datasets = build_microbatched_datasets_tags(
+    pinfo.EndpointDirection.inp, partition_info, num_micro_batches)
+  send_tag_datasets = build_microbatched_datasets_tags(
+    pinfo.EndpointDirection.out, partition_info, num_micro_batches)
+  seq_input_dataset =  build_microbatched_datasets_seq()
+  
+  real_output_datasets = build_microbatched_datasets_real(
+    pinfo.EndpointType.out, labels, num_micro_batches, micro_batch_size)
+  edge_output_datasets = build_microbatched_datasets_edge(
+    pinfo.EndpointType.out_edge, partition_info, num_micro_batches, micro_batch_size)
+  seq_output_dataset =  build_microbatched_datasets_seq()
+  
+  # create generator function that implements an iterator over
+  # a pair of `inputs`/`outputs` dict's
+  # that map from layer names to individual samples
   def generator():
-    for inout in zip(*input_datasets, *recv_tag_datasets, *send_tag_datasets,
-                     *start_seq_dataset, *output_datasets, *end_seq_dataset):
+    for inout in zip(*real_input_datasets, *edge_input_datasets,
+                     *recv_tag_datasets, *send_tag_datasets,
+                     *seq_input_dataset,
+                     *real_output_datasets, *edge_output_datasets,
+                     *seq_output_dataset):
+
       offset = 0
-      inputs = dict()
-      for i in range(num_inputs):
-        for m in range(num_micro_batches):
-          serial_index = i * num_micro_batches + m
-          inputs[input_names[serial_index]] = inout[serial_index]
-      offset += num_inputs * num_micro_batches
+      size = num_real_inputs * num_micro_batches
+      real_inputs = map_layer_names_to_samples(inout[offset:offset+size], pinfo.EndpointType.inp,
+                                               partition_info.pid, num_real_inputs, num_micro_batches)
+      offset += size
 
-      for i in range(num_recvs):
-        for m in range(num_micro_batches):
-          serial_index = i * num_micro_batches + m
-          inputs[recv_tag_names[serial_index]] = inout[offset + serial_index]
-      offset += num_recvs * num_micro_batches
+      size = num_edge_inputs * num_micro_batches
+      edge_inputs = map_layer_names_to_samples(inout[offset:offset+size], pinfo.EndpointType.inp_edge,
+                                               partition_info.pid, num_edge_inputs, num_micro_batches)
+      offset += size
 
-      for i in range(num_sends):
-        for m in range(num_micro_batches):
-          serial_index = i * num_micro_batches + m
-          inputs[send_tag_names[serial_index]] = inout[offset + serial_index]
-      offset += num_sends * num_micro_batches
+      size = num_edge_inputs * num_micro_batches
+      recv_tags = map_layer_names_to_samples(inout[offset:offset+size], pinfo.EndpointType.recv_tag,
+                                             partition_info.pid, num_edge_inputs, num_micro_batches)
+      offset += size
 
-      inputs[start_seq_name] = inout[offset]
-      offset += 1
+      size = num_edge_outputs * num_micro_batches
+      send_tags = map_layer_names_to_samples(inout[offset:offset+size], pinfo.EndpointType.send_tag,
+                                             partition_info.pid, num_edge_outputs, num_micro_batches)
+      offset += size
 
-      outputs = dict()
-      for o in range(num_outputs):
-        for m in range(num_micro_batches):
-          serial_index = o * num_micro_batches + m
-          outputs[output_names[serial_index]] = inout[offset+serial_index]
-      offset += num_outputs * num_micro_batches
+      size = 1
+      seq_input = map_layer_names_to_samples(inout[offset:offset+size], pinfo.EndpointType.seq_input,
+                                             partition_info.pid)
+      offset += size
 
-      outputs[end_seq_name] = inout[offset]
+      size = num_real_outputs * num_micro_batches
+      real_outputs = map_layer_names_to_samples(inout[offset:offset+size], pinfo.EndpointType.out,
+                                                partition_info.pid, num_real_outputs, num_micro_batches)
+      offset += size
+
+      size = num_edge_outputs * num_micro_batches
+      edge_outputs = map_layer_names_to_samples(inout[offset:offset+size], pinfo.EndpointType.out_edge,
+                                                partition_info.pid, num_edge_outputs, num_micro_batches)
+      offset += size
+
+      size = 1
+      seq_output = map_layer_names_to_samples(inout[offset:offset+size], pinfo.EndpointType.seq_output,
+                                              partition_info.pid)
+
+      inputs = real_inputs
+      inputs.update(edge_inputs)
+      inputs.update(recv_tags)
+      inputs.update(send_tags)
+      inputs.update(seq_input)
+      outputs = real_outputs
+      outputs.update(edge_outputs)
+      outputs.update(seq_output)
       yield (inputs, outputs)
 
-  input_types = {name : input_datasets[index].element_spec.dtype
-                 for index, name in enumerate(input_names)}
-  input_types.update({name : recv_tag_datasets[index].element_spec.dtype
-                      for index, name in enumerate(recv_tag_names)})
-  input_types.update({name : send_tag_datasets[index].element_spec.dtype
-                      for index, name in enumerate(send_tag_names)})
-  input_types.update({start_seq_name : start_seq_dataset[0].element_spec.dtype})
-  output_types = {name : output_datasets[index].element_spec.dtype
-                  for index, name in enumerate(output_names)}
-  output_types.update({end_seq_name : end_seq_dataset[0].element_spec.dtype})
+  input_types = map_layer_names_to_tensor_types(
+    real_input_datasets, pinfo.EndpointType.inp, partition_info.pid, num_micro_batches)
+  input_types.update(
+    map_layer_names_to_tensor_types(
+      edge_input_datasets, pinfo.EndpointType.inp_edge, partition_info.pid, num_micro_batches))
+  input_types.update(
+    map_layer_names_to_tensor_types(
+      recv_tag_datasets, pinfo.EndpointType.recv_tag, partition_info.pid, num_micro_batches))
+  input_types.update(
+    map_layer_names_to_tensor_types(
+      send_tag_datasets, pinfo.EndpointType.send_tag, partition_info.pid, num_micro_batches))
+  input_types.update(
+    map_layer_names_to_tensor_types(
+      seq_input_dataset, pinfo.EndpointType.seq_input, partition_info.pid))
+
+  output_types = map_layer_names_to_tensor_types(
+    real_output_datasets, pinfo.EndpointType.out, partition_info.pid, num_micro_batches)
+  output_types.update(
+    map_layer_names_to_tensor_types(
+      edge_output_datasets, pinfo.EndpointType.out_edge, partition_info.pid, num_micro_batches))
+  output_types.update(
+    map_layer_names_to_tensor_types(
+      seq_output_dataset, pinfo.EndpointType.seq_output, partition_info.pid))
 
   return tf.data.Dataset.from_generator(generator, output_types=(input_types, output_types))
+
+def build_microbatched_datasets_real(endpoint_type, datasets, num_micro_batches, micro_batch_size):
+  microbatched_datasets = list()
+  for i in range(len(datasets)):
+    for m in range(num_micro_batches):
+      microbatched_dataset = datasets[i].batch(micro_batch_size).shard(num_micro_batches, m)
+      microbatched_datasets.append(microbatched_dataset)
+  return microbatched_datasets
+
+def build_microbatched_datasets_edge(endpoint_type, partition_info, num_micro_batches, micro_batch_size):
+  microbatched_datasets = list()
+  edge_data_value = 0.0
+  for edge_info in partition_info.get_infos(endpoint_type):
+    dataset = tf.data.Dataset.from_tensors(
+      tf.constant(edge_data_value, shape=edge_info.shape[1:], dtype=edge_info.dtype))
+    # FIXME:
+    # Need to replace `repeat()` with finite version for "middle" partitions
+    dataset = dataset.repeat().batch(micro_batch_size)
+    microbatched_datasets += num_micro_batches * [dataset]
+  return microbatched_datasets
+
+def build_microbatched_datasets_tags(endpoint_direction, partition_info, num_micro_batches):
+  microbatched_datasets = list()
+  dtype_tags = tf.int32 
+  tags_per_microbatch = 1
+  for connection_id in partition_info.get_edge_ids(endpoint_direction):
+    for m in range(num_micro_batches):
+      tag = tf.constant([m, connection_id], dtype=dtype_tags)
+      microbatched_dataset = tf.data.Dataset.from_tensors(tag).batch(tags_per_microbatch).repeat()
+      microbatched_datasets.append(microbatched_dataset)
+  return microbatched_datasets
+
+def build_microbatched_datasets_seq():
+  dtype_seq_input = tf.float32 
+  seq_per_microbatch = 1
+  seq_data_value = 0.0
+  seq_data = tf.constant(seq_data_value, dtype=dtype_seq_input)
+  return [tf.data.Dataset.from_tensors(seq_data).batch(seq_per_microbatch).repeat()]
+
+def map_layer_names_to_samples(samples, endpoint_type, partition_id, num_inputs = None, num_micro_batches = None):
+  if endpoint_type in [pinfo.EndpointType.seq_input, pinfo.EndpointType.seq_output]:
+    name = create_name_micro_batched_layer(partition_id, endpoint_type)
+    return {name : samples[0]}
+
+  mappings = dict()
+  index = 0
+  for i in range(num_inputs):
+    for m in range(num_micro_batches):
+      name = create_name_micro_batched_layer(partition_id, endpoint_type, layer_id = i, micro_batch_id = m)
+      mappings[name] = samples[index]
+      index += 1
+  return mappings
+
+def map_layer_names_to_tensor_types(datasets, endpoint_type, partition_id, num_micro_batches = None):
+  if endpoint_type in [pinfo.EndpointType.seq_input, pinfo.EndpointType.seq_output]:
+    name = create_name_micro_batched_layer(partition_id, endpoint_type)
+    return {name : datasets[0].element_spec.dtype}
+
+  mappings = dict()
+  for i in range(len(datasets)):
+    for m in range(num_micro_batches):
+      name = create_name_micro_batched_layer(partition_id, endpoint_type, layer_id = i, micro_batch_id = m)
+      mappings[name] = datasets[i].element_spec.dtype
+  return mappings
