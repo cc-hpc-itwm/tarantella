02b4633705ee5c3ca608d34ee9a1eeafbf3436ba
Alexandra <carpenamarie@itwm.fraunhofer.de>
2021-04-13 17:27:03 +0200

    tests: pytest: add test to clone `tnt.Model`s

--- test/python/model_api/model_clone.py
+++ test/python/model_api/model_clone.py
@@ -12,8 +12,12 @@ def keras_model(request):
   yield request.param()
 
 class TestCloneModel:
-  def test_clone_model(self, keras_model):
+  def test_clone_keras_model(self, keras_model):
     cloned_model = tnt.models.clone_model(keras_model)
     tnt_model = tnt.Model(keras_model)
     util.check_model_configuration_identical(tnt_model, cloned_model)
 
+  def test_clone_tnt_model(self, keras_model):
+    tnt_model = tnt.Model(keras_model)
+    cloned_model = tnt.models.clone_model(tnt_model)
+    util.check_model_configuration_identical(tnt_model, cloned_model)
