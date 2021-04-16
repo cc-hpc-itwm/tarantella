ba8b6f67b93bb343343cc4d1f41544f3e4caf1cd
Alexandra <carpenamarie@itwm.fraunhofer.de>
2021-04-16 19:52:09 +0200

    CMake: update `cleanup` test to run on multiple ranks

--- cmake/cleanup.sh
+++ cmake/cleanup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-procs=`ps aux | grep --regexp="\(py\)\?test" | grep -v cleanup.sh | grep -v ctest | grep -v grep`
+procs=`ps aux | grep --regexp="\(py\)\?[Tt]est" | grep -v cleanup.sh | grep -v ctest | grep -v grep  | grep -v run_`
 if [ -n "$procs" ] ;then
-  ps aux | grep --regexp="\(py\)\?test" | grep -v cleanup.sh | grep -v ctest | grep -v grep | awk '{print $2}' | xargs kill || 1 2>&1 > /dev/null 
+  ps aux | grep --regexp="\(py\)\?[Tt]est" | grep -v cleanup.sh | grep -v ctest | grep -v grep | grep -v run_ | awk '{print $2}' | xargs kill || 1 2>&1 > /dev/null
 fi
+
+exit 0
