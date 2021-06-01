ae47d491f4646e984aafa0131c624c5f833da6b9
Pengqiu Li <pengqiu.li@itwm.fraunhofer.de>
2021-06-01 17:00:20 +0200

    Pytest: Accuracy test: Check both history and evaluate result in accuracy test with extra sample.

--- test/python/data_parallel_training/accuracy_test_with_extra_sample.py
+++ test/python/data_parallel_training/accuracy_test_with_extra_sample.py
@@ -19,20 +19,20 @@ def model_runners(request):
 
 class TestsDataParallelCompareAccuracy:
   @pytest.mark.skipif(tf.version.VERSION < "2.2.0",reason="requires tf >= 2.2")
-  @pytest.mark.parametrize("micro_batch_size", [64])
-  @pytest.mark.parametrize("number_epochs", [4])
-  @pytest.mark.parametrize("nbatches", [15])
+  @pytest.mark.parametrize("micro_batch_size", [32])
+  @pytest.mark.parametrize("number_epochs", [2])
+  @pytest.mark.parametrize("nbatches", [10])
   @pytest.mark.parametrize("test_nbatches", [2])
-  @pytest.mark.parametrize("extra_batch", [0, 5, 7])
-  @pytest.mark.parametrize("extra_sample", [5, 7])
+  @pytest.mark.parametrize("extra_batch", [0, 7])
+  @pytest.mark.parametrize("extra_sample", [2, 6, 23])
   def test_compare_accuracy_against_reference_with_pad(self, model_runners, micro_batch_size,
                                                            number_epochs, nbatches, test_nbatches,
                                                            extra_batch, extra_sample):
-    (train_dataset, _) = util.train_test_mnist_datasets(nbatches, test_nbatches,
+    (train_dataset, test_dataset) = util.train_test_mnist_datasets(nbatches, test_nbatches,
                                                                    micro_batch_size,
                                                                    extra_batch = extra_batch,
                                                                    extra_sample = extra_sample)
-    (ref_train_dataset, _) = util.train_test_mnist_datasets(nbatches, test_nbatches,
+    (ref_train_dataset, ref_test_dataset) = util.train_test_mnist_datasets(nbatches, test_nbatches,
                                                                            micro_batch_size,
                                                                            extra_batch = extra_batch,
                                                                            extra_sample = extra_sample)
@@ -46,3 +46,13 @@ class TestsDataParallelCompareAccuracy:
     
     assert np.allclose(tnt_history.history['loss'], ref_history.history['loss'], tolerate)
     assert np.allclose(tnt_history.history['sparse_categorical_accuracy'], ref_history.history['sparse_categorical_accuracy'], tolerate)
+    
+    tnt_loss_accuracy = tnt_model_runner.evaluate_model(test_dataset)
+    reference_loss_accuracy = reference_model_runner.evaluate_model(ref_test_dataset)
+
+    rank = tnt.get_rank()
+    logging.getLogger().info("[Rank %d] Tarantella[loss, accuracy] = %s" % (rank, str(tnt_loss_accuracy)))
+    logging.getLogger().info("[Rank %d] Reference [loss, accuracy] = %s" % (rank, str(reference_loss_accuracy)))
+
+    assert np.isclose(tnt_loss_accuracy[0], reference_loss_accuracy[0], atol=1e-2) # losses might not be identical
+    assert np.isclose(tnt_loss_accuracy[1], reference_loss_accuracy[1], atol=1e-6)
