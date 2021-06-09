e2b0426da5fbabbae63041249133e7ebd9014036
Alexandra <carpenamarie@itwm.fraunhofer.de>
2021-06-09 16:49:32 +0200

    tarantella: datasets: gradient scaling callback: update scaling factor only when it changes

--- src/tarantella/datasets/gradient_scaling_callback.py
+++ src/tarantella/datasets/gradient_scaling_callback.py
@@ -1,18 +1,12 @@
 import tensorflow as tf
 from tensorflow.python.keras import backend as K
 
+import tarantella as tnt
 from tarantella import logger
 import tarantella.datasets.dataset_helpers as helpers
 
 def _get_scaling_factor(micro_batch_size, batch_size, num_ranks):
   return micro_batch_size * num_ranks / batch_size
-
-def get_scaling_factor_by_iteration(iteration_id, scaling_factor_table):
-  scaling_factor = 1.0
-  for min_iteration_id, value in sorted(scaling_factor_table.items()):
-    if iteration_id >= min_iteration_id:
-      scaling_factor = value
-  return scaling_factor
 
 def build_scaling_factor_table(rank, num_ranks, num_samples, batch_size):
   # Defines the gradient `scaling_factor` to be used for each iteration starting
@@ -41,16 +35,25 @@ def build_scaling_factor_table(rank, num_ranks, num_samples, batch_size):
     scaling_factor_table[final_iteration_id] = last_iteration_scaling_factor
   return scaling_factor_table
 
+def get_scaling_factor_by_iteration(iteration_id, scaling_factor_table):
+  scaling_factor = 1.0
+  for min_iteration_id, value in sorted(scaling_factor_table.items()):
+    if iteration_id >= min_iteration_id:
+      scaling_factor = value
+  return scaling_factor
+
 class ScalingFactorScheduler(tf.keras.callbacks.Callback):
-  def __init__(self, scaling_factor_table, get_scaling_factor_by_iteration):
+  def __init__(self, scaling_factor_table):
     super().__init__()
-    self.get_scaling_factor_by_iteration = get_scaling_factor_by_iteration
 
     self.scaling_factor_table = dict()
     for iteration_id, scaling_factor in scaling_factor_table.items():
       self.scaling_factor_table[iteration_id] = tf.dtypes.cast(scaling_factor, tf.float32)
 
   def on_train_batch_begin(self, batch, logs=None):
-    K.set_value(self.model.optimizer.scaling_factor,
-                self.get_scaling_factor_by_iteration(batch, self.scaling_factor_table))
+    scaling_factor = get_scaling_factor_by_iteration(batch, self.scaling_factor_table)
+    if scaling_factor != self.model.optimizer.scaling_factor:
+      logger.debug(f"[rank {tnt.get_rank()}] Setting scaling factor to {scaling_factor}")
+      K.set_value(self.model.optimizer.scaling_factor, scaling_factor)
+
 
