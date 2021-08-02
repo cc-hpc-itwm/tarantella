461b6482309c0b941c27bd2d554fb00ca0e381a2
Alexandra <carpenamarie@itwm.fraunhofer.de>
2021-08-02 14:01:31 +0200

    tnt: collectives: utils: add `is_scalar` function
    * remove underscore from function names to match our regular naming scheme

--- src/tarantella/collectives/TensorAllreducer.py
+++ src/tarantella/collectives/TensorAllreducer.py
@@ -6,55 +6,55 @@ import numpy as np
 
 class TensorAllreducer:
   def __init__(self, inputs):
-    if utils.__is_nonEmptyDict__(inputs):
+    if utils.is_nonEmptyDict(inputs):
       self.allreducer = dict()
       for key in inputs.keys():
         self.allreducer[key] = TensorAllreducer(inputs[key])
       return
 
     default_tensor_id = 0
-    if utils.__is_floatOrDouble__(inputs) or utils.__is_int__(inputs) or utils.__is_tensor__(inputs):
+    if utils.is_scalar(inputs) or utils.is_tensor(inputs):
       self.shapes = [tf.shape(inputs)]
       tensor_infos = [utils.get_tensor_info(default_tensor_id, np.asarray(inputs))]
-    elif utils.__is_nonEmptyArray__(inputs):
+    elif utils.is_nonEmptyArray(inputs):
       self.shapes = [inputs.shape]
       tensor_infos = [utils.get_tensor_info(default_tensor_id, inputs)]
-    elif utils.__is_nonEmptyList__(inputs):
+    elif utils.is_nonEmptyList(inputs):
       self.shapes = [tensor.shape for tensor in inputs]
       tensor_infos = [utils.get_tensor_info(tid, tensor) for tid, tensor in enumerate(inputs)]
     else:
-      self.raise_input_error()
+      self._raise_input_error()
 
     self.allreducer = GPICommLib.TensorAllreducer(tensor_infos)
 
 
   def allreduce(self, inputs):
-    if utils.__is_nonEmptyDict__(inputs):
+    if utils.is_nonEmptyDict(inputs):
       output_dict = dict()
       for key in inputs.keys():
         output_dict[key] = self.allreducer[key].allreduce(inputs[key])
       return output_dict
 
-    if utils.__is_floatOrDouble__(inputs) or utils.__is_int__(inputs):
+    if utils.is_scalar(inputs):
       return self.allreducer.allreduce([np.asarray(inputs)])[0][0]
-    elif utils.__is_tensor__(inputs):
+    elif utils.is_tensor(inputs):
       outputs = self.allreducer.allreduce([np.asarray(inputs)])[0]
       outputs = outputs.reshape(self.shapes[0])
       return tf.convert_to_tensor(outputs)
-    elif utils.__is_nonEmptyArray__(inputs):
+    elif utils.is_nonEmptyArray(inputs):
       outputs = self.allreducer.allreduce([inputs])[0]
       outputs = outputs.reshape(self.shapes[0])
       return outputs
-    elif utils.__is_nonEmptyList__(inputs):
+    elif utils.is_nonEmptyList(inputs):
       outputs = self.allreducer.allreduce(inputs)
       for i, _ in enumerate(outputs):
         outputs[i] = outputs[i].reshape(self.shapes[i])
       return outputs
     else:
-      self.raise_input_error()
+      self._raise_input_error()
 
 
-  def raise_input_error(self):
+  def _raise_input_error(self):
     raise TypeError('[Tarantella][TensorAllreducer] '
                     '`inputs` should be either a dict, list, '
                     'an `np.ndarray` object, or a float/double.')
