1c5b09476d3a7d4cf57830e1edf5ab73e82d1921
Alexandra <carpenamarie@itwm.fraunhofer.de>
2021-08-02 16:33:48 +0200

    tnt: strategy: pipelining: RankMapper: map rank to partition id instead of relying on the `pipeline_graph` directly

--- src/tarantella/strategy/pipelining/rank_mapper.py
+++ src/tarantella/strategy/pipelining/rank_mapper.py
@@ -9,16 +9,28 @@ class RankMapper:
   def __init__(self, pipeline_graph, nranks):
     self.pipeline_graph = pipeline_graph
     self.nranks = nranks
+    self.mapping = self._map_ranks_to_partition_ids()
+
+  def _map_ranks_to_partition_ids(self):
+    assert len(self.pipeline_graph.nodes) <= self.nranks
+    mapping = dict()
+    for index, partition_id in enumerate(list(self.pipeline_graph.nodes)):
+      rank = index
+      mapping[rank] = partition_id
+    return mapping
 
   def get_partition_for_rank(self, rank):
-    assert rank < self.nranks
-    return list(self.pipeline_graph.nodes)[rank]
+    if rank in self.mapping.keys():
+      return self.mapping[rank]
+    raise ValueError(f"[get_partition_for_rank] No partition id is mapped to rank {rank}:")
 
-  def get_rank_for_partition(self, partition_name):
-    for index, pname in enumerate(list(self.pipeline_graph.nodes)):
-      if pname == partition_name:
-        return index # one partition per rank
-    raise RuntimeError(f"[get_rank_for_partition] Unknown partition name {partition_name}")
+  def get_rank_for_partition(self, partition_id):
+    matching_ranks = [rank for rank, p_id in self.mapping.items() if p_id == partition_id]
+
+    if len(matching_ranks) == 1:
+      return matching_ranks[0]
+    raise ValueError(f"[get_rank_for_partition] Invalid mapping for partition id {partition_id}:"
+                     f"matching ranks = {matching_ranks}")
 
   def get_connections_for_rank(self, rank):
     # Create connections dict: { conn_id: ((rank_i, rank), size_in_bytes)}
