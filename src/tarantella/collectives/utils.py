9a368af3c30f0b725aeeb009e1a4417ffefb604a
Vivek Yadav <vivekyadav.mit@gmail.com>
2021-09-13 23:35:17 +0200

    tarantella: gpicommlib: Pybind wrappers: add int64 support required by TF version 2.0/2.1

--- src/tarantella/collectives/utils.py
+++ src/tarantella/collectives/utils.py
@@ -13,7 +13,7 @@ def is_floatOrDouble(input):
   return isinstance(input, (np.float, np.double, np.float32, np.float64))
 
 def is_int(input):
-  return isinstance(input, (np.int, np.int16, np.int32))
+  return isinstance(input, (np.int, np.int16, np.int32, np.int64))
 
 def is_scalar(input):
   return is_int(input) or is_floatOrDouble(input)
