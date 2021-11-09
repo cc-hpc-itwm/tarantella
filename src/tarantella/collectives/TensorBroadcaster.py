4c045dff5620783d4ed9556428e45e66c8ca0fd8
Alexandra <carpenamarie@itwm.fraunhofer.de>
2021-11-09 20:03:13 +0100

    tnt: collectives: TensorBroadcaster: switch to `pygpi`-based `Broadcast`

--- src/tarantella/collectives/TensorBroadcaster.py
+++ src/tarantella/collectives/TensorBroadcaster.py
@@ -1,48 +1,43 @@
-import GPICommLib
+import pygpi
 
 import tarantella as tnt
 from . import utils
 
+import numpy as np
+
 class TensorBroadcaster():
-  def __init__(self, input, root_rank = tnt.get_master_rank()):
+  def __init__(self, inputs, root_rank = tnt.get_master_rank()):
     self.root_rank = root_rank
     self.shapes = list()
+    self.broadcasts = list()
 
-    if utils.is_nonEmptyList(input):
-      tensor_infos = [utils.get_tensor_info(tid, tensor) for tid, tensor in enumerate(input)]
-      self.shapes = [array.shape for array in input]
-    elif utils.is_nonEmptyArray(input):
-      tensor_infos = [utils.get_tensor_info(0, input)]
-      self.shapes = [input.shape]
-    else:
+    if utils.is_nonEmptyArray(inputs):
+      inputs = [inputs]
+    elif not utils.is_nonEmptyList(inputs):
       raise TypeError("""[Tarantella][TensorBroadcaster] Input should be
                       either a list or an `np.ndarray` object and non-empty.""")
+    for tensor in inputs:
+      self.shapes.append(tensor.shape)
+      self.broadcasts.append(pygpi.Broadcast(pygpi.Group(), int(np.prod(tensor.shape)),
+                                          self.root_rank,
+                                          dtype = tensor.dtype))
 
-    self.broadcaster = GPICommLib.TensorBroadcaster(tensor_infos, self.root_rank)
+  def broadcast(self, inputs = None):
+    outputs = list()
+    for i, bcast in enumerate(self.broadcasts):
+      if pygpi.get_rank() == self.root_rank:
+        if utils.is_nonEmptyArray(inputs):
+          inputs = [inputs]
+        elif not utils.is_nonEmptyList(inputs):
+          raise TypeError("""[Tarantella][TensorBroadcaster] Input should be
+                          either a list or an `np.ndarray` object and non-empty.""")
+        assert len(self.broadcasts) == len(inputs)
 
-  def broadcast(self, input = None):
-    if input is not None: # call with input on root rank
-      if tnt.get_rank() != self.root_rank:
-        raise RuntimeError("[Tarantella][TensorBroadcaster][broadcast] "
-                           "function with input must be called on root rank.")
-      if utils.is_nonEmptyList(input):
-        self.broadcaster.broadcast(input)
-        return input
-      elif utils.is_nonEmptyArray(input):
-        self.broadcaster.broadcast([input])[0]
-        return input
+        bcast.start(inputs[i])
       else:
-        raise TypeError("[Tarantella][TensorBroadcaster][broadcast] "
-                        "Input should be either a list or an `np.ndarray` "
-                        "object and non-empty.")
-    else: # non root ranks
-      if tnt.get_rank() == self.root_rank:
-        raise RuntimeError("[Tarantella][TensorBroadcaster][broadcast] "
-                           "function without input must be called on non-root rank.")
-      outputs = self.broadcaster.broadcast([])
-      for i in range(len(outputs)):
-        outputs[i] = outputs[i].reshape(self.shapes[i])
-      if len(outputs) == 1:
-        return outputs[0]
-      else:
-        return outputs
+        bcast.start()
+    for i, bcast in enumerate(self.broadcasts):
+      out = bcast.wait_for_completion()
+      outputs.append(out.reshape(self.shapes[i]))
+    return outputs if len(outputs) > 1 else outputs[0]
+    
