dd848a43cab9abb36087a62e140b963f115a3384
Alexandra <carpenamarie@itwm.fraunhofer.de>
2021-12-14 18:45:47 +0100

    tnt: Tf ops: Allgather

--- src/gpi_comm_lib/tf_ops/AllgatherOps.cpp
+++ src/gpi_comm_lib/tf_ops/AllgatherOps.cpp
@@ -10,20 +10,29 @@ using namespace tensorflow;
 
 REGISTER_OP("AllgatherOp")
     .Attr("tnt_gatherer: int")
+    .Attr("batch_size: int")
     .Input("input_tensor: float")
-    .Input("output_shape: int32")
     .Output("output_tensor: float")
     .SetShapeFn([](::tensorflow::shape_inference::InferenceContext *c)
     {
-      c->set_output(0, c->input(1)); // set shape of output
+      int batch_size;
+      c->GetAttr("batch_size", &batch_size);
+
+      shape_inference::ShapeHandle sample_shape;
+      TF_RETURN_IF_ERROR(c->Subshape(c->input(0), 1, &sample_shape));
+
+      shape_inference::ShapeHandle batch_shape = c->MakeShape({batch_size});
+      shape_inference::ShapeHandle out_shape;
+
+      TF_RETURN_IF_ERROR(c->Concatenate(batch_shape, sample_shape, &out_shape));
+      c->set_output(0, out_shape);
       return Status::OK();
     });
 
-template <typename Allgatherimplementation>
-class AllgatherTensorOp : public OpKernel
+class AllgatherOp : public OpKernel
 {
   public:
-    explicit AllgatherTensorOp(OpKernelConstruction* context)
+    explicit AllgatherOp(OpKernelConstruction* context)
         : OpKernel(context)
     {
       tensorflow::int64 context_ptr;
@@ -32,30 +41,14 @@ class AllgatherTensorOp : public OpKernel
       allgatherer = reinterpret_cast<tarantella::TensorAllgatherver *>(context_ptr);
     }
 
-    void Compute(OpKernelContext* context) override
-    {
-      static_cast<Allgatherimplementation&>(*this).compute_impl(context);
-    }
-
-  protected:
-    tarantella::TensorAllgatherver *allgatherer;
-};
-
-class AllgatherOp : public AllgatherTensorOp<AllgatherOp>
-{
-  public:
-    using AllgatherTensorOp<AllgatherOp>::AllgatherTensorOp;
-
-    void compute_impl(OpKernelContext* context)
+    void Compute(OpKernelContext* context)
     {
       const Tensor& input_tensor = context->input(0);
       auto* inputs = input_tensor.flat<float>().data();
 
       // construct shape using output size
-      const Tensor& output_size_tensor = context->input(1);
-      auto* output_size = output_size_tensor.flat<int>().data();
-
-      TensorShape output_shape({*output_size, 1});
+      long long int size = allgatherer->getOutputCount();
+      TensorShape output_shape{size};
 
       //allocate output
       auto const output_index = 0;
@@ -65,9 +58,11 @@ class AllgatherOp : public AllgatherTensorOp<AllgatherOp>
                                                        output_shape,
                                                        &output_tensor));
       auto* outputs = output_tensor->flat<float>().data();
-
       allgatherer->exec_allgatherv(inputs, outputs);
     }
+
+  private:
+    tarantella::TensorAllgatherver *allgatherer;
 };
 
 REGISTER_KERNEL_BUILDER(Name("AllgatherOp").Device(DEVICE_CPU), AllgatherOp);
