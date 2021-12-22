1f39165b8e050a19325edbf2f6f5811275952e77
Alexandra <carpenamarie@itwm.fraunhofer.de>
2021-12-22 15:39:07 +0100

    tnt: keras: metrics: support all TF versions in custom metrics

--- src/tarantella/keras/metrics.py
+++ src/tarantella/keras/metrics.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
+import tarantella.utilities.tf_version as version_utils
 
 class ZeroMetric(tf.keras.metrics.Metric):
 
   def __init__(self, name='zero_metric', **kwargs):
     super(ZeroMetric, self).__init__(name=name, **kwargs)
     self.zero = self.add_weight(name='tp', initializer='zeros')
+    self._add_support_for_deprecated_methods()
 
   def update_state(self, y_true, y_pred, sample_weight=None):
     y_true = tf.cast(y_true, tf.bool)
@@ -14,5 +16,9 @@ class ZeroMetric(tf.keras.metrics.Metric):
   def result(self):
     return self.zero
 
-  def reset_states(self):
+  def reset_state(self):
     self.zero.assign(0)
+
+  def _add_support_for_deprecated_methods(self):
+    if version_utils.tf_version_below_equal('2.4'):
+      self.reset_states = self.reset_state
