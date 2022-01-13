ac07f51023bc5e97e290258f78ad262ee7535d73
Alexandra <carpenamarie@itwm.fraunhofer.de>
2022-01-13 14:51:58 +0100

    tnt: strategy: pipelining: RankMapper: create groups for pipelining communication and for data parallel communication for a given rank

--- src/tarantella/strategy/pipelining/rank_mapper.py
+++ src/tarantella/strategy/pipelining/rank_mapper.py
@@ -1,17 +1,30 @@
+import tarantella as tnt
 import tarantella.strategy.pipelining.connection_info as cinfo
 
 class RankMapper:
+  # Defines a mapping between ranks and a tuple (partition_id, replica_id)
   # Arguments:
-  # - pipeline_graph: graphs.MultiDirectedGraph with
-  #   partition names as nodes and connections as edges
-  # - nranks: total number of ranks
-  def __init__(self, pipeline_graph, nranks):
+  # - num_ranks: total number of ranks
+  # - num_partitions
+  def __init__(self, num_ranks, num_partitions, pipeline_graph):
+    self.num_partitions = num_partitions
+    self.num_ranks = num_ranks
     self.pipeline_graph = pipeline_graph
-    self.nranks = nranks
     self.mapping = self._map_ranks_to_partition_ids()
 
+  def get_groups_for_rank(self, rank):
+    replica_index = rank // self.num_partitions
+    first_rank_in_partition = replica_index * self.num_partitions
+    pipeline_group_ranks = range(first_rank_in_partition, first_rank_in_partition + self.num_partitions)
+
+    nranks_dp = self.num_ranks // self.num_partitions if self.num_partitions > 1 else self.num_ranks
+    first_rank_in_dp = rank - first_rank_in_partition
+    dp_group_ranks = range(first_rank_in_dp, first_rank_in_dp + nranks_dp, self.num_partitions)
+
+    return tnt.Group(pipeline_group_ranks), tnt.Group(dp_group_ranks)
+
   def _map_ranks_to_partition_ids(self):
-    assert len(self.pipeline_graph.get_nodes()) <= self.nranks
+    assert len(self.pipeline_graph.get_nodes()) <= self.num_ranks
     mapping = dict()
     for index, node in enumerate(self.pipeline_graph.get_nodes()):
       rank = index
@@ -34,7 +47,7 @@ class RankMapper:
 
   def get_connections_for_rank(self, rank):
     # Create connections dict: { conn_id: ((rank_i, rank), size_in_bytes)}
-    assert rank < self.nranks
+    assert rank < self.num_ranks
     connection_table = dict()
     for edge in self.pipeline_graph.get_edges():
       conn_id = edge.info_dict['connection_id']
