1e5a36e018272e5e277ff6df9e08ff8c9cfebbc5
Alexandra <carpenamarie@itwm.fraunhofer.de>
2022-02-03 14:20:10 +0100

    tnt: keras: Model: instantiate model type dynamically based on the required parallelization strategy

--- src/tarantella/keras/model.py
+++ src/tarantella/keras/model.py
@@ -18,35 +18,49 @@ import atexit
 # Model parallelism not supportted for TF version < 2.3
 TF_DEFAULT_PIPELINING_FLAG = (version_utils.tf_version_above_equal('2.3'))
 
-def Model(model,
-          enable_data_parallelism = True,
-          enable_model_parallelism = TF_DEFAULT_PIPELINING_FLAG,
-          num_pipeline_stages = 1):
-  replica_group = tnt.Group()
+class ModelMeta(type):
+  def __call__(cls, *args, **kwargs):
+    obj = cls._create_tnt_model(*args, **kwargs)
+    return obj
 
-  if enable_model_parallelism and isinstance(model, tf.keras.Sequential):
-    logger.warn(f"Cannot pipeline a `tf.keras.Sequential` model; disabling model parallelism.")
-    enable_model_parallelism = False
+  def _create_tnt_model(cls, model,
+              enable_data_parallelism = True,
+              enable_model_parallelism = TF_DEFAULT_PIPELINING_FLAG,
+              num_pipeline_stages = 1):
+    replica_group = tnt.Group()
 
-  if enable_model_parallelism:
-    rank = tnt.get_rank()
+    if enable_model_parallelism and isinstance(model, tf.keras.Sequential):
+      logger.warn(f"Cannot pipeline a `tf.keras.Sequential` model; disabling model parallelism.")
+      enable_model_parallelism = False
 
-    partition_generator = pgen.GraphPartitionGenerator(model)
-    rank_mapper = rmapper.RankMapper(num_ranks = tnt.get_size(),
-                                    pipeline_graph = partition_generator.get_pipeline_graph())
-    pipeline_group = rank_mapper.get_pipelining_group_for_rank(rank)
+    if enable_model_parallelism:
+      rank = tnt.get_rank()
 
-    logger.info(f"Creating pipelined model with {pipeline_group.size} partitions.")
-    # get my partition
-    model = pm.PartitionedModel(model, pipeline_group, partition_generator, rank_mapper,
-                                num_pipeline_stages)
-    replica_group = rank_mapper.get_replica_group_for_rank(rank)
-  
-  if enable_data_parallelism:
-    # replicate my partition across the data parallel group
-    logger.info(f"Replicating local model across {replica_group.group} ranks.")
-    model = DataParallelModel(model, group = replica_group)
-  return model
+      partition_generator = pgen.GraphPartitionGenerator(model)
+      rank_mapper = rmapper.RankMapper(num_ranks = tnt.get_size(),
+                                      pipeline_graph = partition_generator.get_pipeline_graph())
+      pipeline_group = rank_mapper.get_pipelining_group_for_rank(rank)
+
+      logger.info(f"Creating pipelined model with {pipeline_group.size} partitions.")
+      # get my partition
+      model = pm.PartitionedModel(model = model, group = pipeline_group,
+                                  partition_generator = partition_generator, rank_mapper = rank_mapper,
+                                  num_pipeline_stages=num_pipeline_stages)
+      replica_group = rank_mapper.get_replica_group_for_rank(rank)
+
+    if enable_data_parallelism:
+      # replicate my partition across the data parallel group
+      logger.info(f"Replicating local model across {replica_group.group} ranks.")
+      model = DataParallelModel(model = model, group = replica_group)
+    return model
+
+
+class Model(metaclass=ModelMeta):
+  @classmethod
+  def from_config(cls, *args, **kwargs):
+    return DataParallelModel.from_config(*args, **kwargs)
+
+
 
 class DataParallelModel(tf.keras.models.Model):
   def __init__(self, model, group = tnt.Group()):
