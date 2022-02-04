35e9ea0181fc9d35672c89780f5b392af69327a0
Alexandra <carpenamarie@itwm.fraunhofer.de>
2022-02-04 18:20:59 +0100

    tests: pytest: model configuration: specifically test models with and without pipelining
    * remove the requirement for models to be of type `tnt.Model`
    (as now they can be `DataParallelModel`, `PartitionedModel` or both)

--- test/python/model_api/model_save_load_configuration.py
+++ test/python/model_api/model_save_load_configuration.py
@@ -12,43 +12,39 @@ import yaml
 def model(request):
   yield request.param()
 
-class TestsModelSaveLoadInMemory:
-  def test_model_get_config(self, model):
-    tnt_model = tnt.Model(model)
+@pytest.mark.parametrize("enable_model_parallelism", [False,
+                                                      True])
+class TestModelSaveLoadInMemory:
+  def test_model_get_config(self, model, enable_model_parallelism):
+    tnt_model = tnt.Model(model, enable_model_parallelism = enable_model_parallelism)
     assert tnt_model.get_config() == model.get_config()
 
-  def test_model_to_json(self, model):
-    tnt_model = tnt.Model(model)
+  def test_model_to_json(self, model, enable_model_parallelism):
+    tnt_model = tnt.Model(model, enable_model_parallelism = enable_model_parallelism)
     assert tnt_model.to_json() == model.to_json()
 
   @pytest.mark.max_tfversion('2.5') # method deprecated later
-  def test_model_to_yaml(self, model):
-    tnt_model = tnt.Model(model)
+  def test_model_to_yaml(self, model, enable_model_parallelism):
+    tnt_model = tnt.Model(model, enable_model_parallelism = enable_model_parallelism)
     assert tnt_model.to_yaml() == model.to_yaml()
 
-  def test_model_from_config(self, model):
-    tnt_model = tnt.Model(model)
+  def test_model_from_config(self, model, enable_model_parallelism):
+    tnt_model = tnt.Model(model, enable_model_parallelism = enable_model_parallelism)
     config = tnt_model.get_config()
     model_from_config = tnt.models.model_from_config(config)
-
-    assert isinstance(model_from_config, tnt.Model)
     util.check_model_configuration_identical(model_from_config, model)
 
-  def test_model_from_json(self, model):
-    tnt_model = tnt.Model(model)
+  def test_model_from_json(self, model, enable_model_parallelism):
+    tnt_model = tnt.Model(model, enable_model_parallelism = enable_model_parallelism)
     json = tnt_model.to_json()
     json_model = tnt.models.model_from_json(json)
-
-    assert isinstance(json_model, tnt.Model)
     util.check_model_configuration_identical(json_model, model)
 
   @pytest.mark.max_tfversion('2.5') # method deprecated later
-  def test_model_from_yaml(self, model):
-    tnt_model = tnt.Model(model)
+  def test_model_from_yaml(self, model, enable_model_parallelism):
+    tnt_model = tnt.Model(model, enable_model_parallelism = enable_model_parallelism)
     yaml = tnt_model.to_yaml()
     yaml_model = tnt.models.model_from_yaml(yaml)
-
-    assert isinstance(yaml_model, tnt.Model)
     util.check_model_configuration_identical(yaml_model, model)
 
 
@@ -56,11 +52,11 @@ class TestsModelSaveLoadInMemory:
 def sequential(request):
   yield request.param()
 
-class TestsSequentialSaveLoadInMemory(TestsModelSaveLoadInMemory):
-  def test_model_from_config(self, sequential):
-    tnt_model = tnt.Model(sequential)
+@pytest.mark.parametrize("enable_model_parallelism", [False,
+                                                      pytest.param(True, marks=pytest.mark.xfail),])
+class TestSequentialSaveLoadInMemory:
+  def test_model_from_config(self, sequential, enable_model_parallelism):
+    tnt_model = tnt.Model(sequential, enable_model_parallelism = enable_model_parallelism)
     config = tnt_model.get_config()
     model_from_config = tnt.Sequential.from_config(config)
-
-    assert isinstance(model_from_config, tnt.Model)
     util.check_model_configuration_identical(model_from_config, sequential)
