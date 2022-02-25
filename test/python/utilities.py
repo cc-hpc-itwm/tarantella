d2ba542ff0e1f335117201d5735ace1dc7857c28
Alexandra <carpenamarie@itwm.fraunhofer.de>
2022-02-25 11:31:14 +0100

    tests: pytest: dataset helpers: apply shuffle before batching

--- test/python/utilities.py
+++ test/python/utilities.py
@@ -12,10 +12,10 @@ import random
 import csv
 import re
 
-def create_dataset_from_arrays(samples, labels, batch_size,drop_remainder = False):
+def create_dataset_from_arrays(samples, labels):
   assert(len(samples) == len(labels))
   ds = tf.data.Dataset.from_tensor_slices((samples, labels))
-  return ds.batch(batch_size,drop_remainder = drop_remainder)
+  return ds
 
 def load_dataset(dataset_loader,
                  train_size, train_batch_size,
@@ -25,13 +25,14 @@ def load_dataset(dataset_loader,
   shuffle_seed = 1234
 
   (x_train, y_train), (x_val, y_val), (x_test, y_test) = dataset_loader(train_size, 0, test_size)
-  train_dataset = create_dataset_from_arrays(x_train, y_train, train_batch_size, drop_remainder = drop_remainder)
-  test_dataset = create_dataset_from_arrays(x_test, y_test, test_batch_size)
+  train_dataset = create_dataset_from_arrays(x_train, y_train)
+  test_dataset = create_dataset_from_arrays(x_test, y_test)
 
   if shuffle:
-    train_dataset = train_dataset.shuffle(len(x_train), shuffle_seed,
+    train_dataset = train_dataset.shuffle(len(x_train), seed = shuffle_seed,
                                           reshuffle_each_iteration = False)
-  return (train_dataset, test_dataset)
+  return train_dataset.batch(train_batch_size,drop_remainder = drop_remainder), \
+         test_dataset.batch(test_batch_size,drop_remainder = drop_remainder)
 
 def train_test_mnist_datasets(nbatches = 1, test_nbatches = 0,
                               micro_batch_size = 64, shuffle = True, 
