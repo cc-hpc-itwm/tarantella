651ff8b5033994b95dfd96492fadcadf288d260a
Alexandra <carpenamarie@itwm.fraunhofer.de>
2022-03-01 17:50:24 +0100

    tests: pytest: callbacks: refactor tests, add pipelined model
    * `ModelCheckpoint` is a special case, as checkpointing not defined yet for partitioned models

--- test/python/callbacks_test.py
+++ test/python/callbacks_test.py
@@ -3,6 +3,7 @@
 #   Modifications Copyright (C) 2022 Fraunhofer ITWM <http://www.itwm.fraunhofer.de/>
 
 from models import mnist_models as mnist
+from tarantella.strategy.parallel_strategy import ParallelStrategy
 import training_runner as base_runner
 import utilities as util
 import tarantella as tnt
@@ -16,15 +17,6 @@ import numpy as np
 import os
 import pytest
 import shutil
-
-@pytest.fixture(scope="function", params=[base_runner.ModelConfig(mnist.fc_model_generator, True),
-                                          base_runner.ModelConfig(mnist.fc_model_generator, False),
-                                          base_runner.ModelConfig(mnist.subclassed_model_generator)])
-def model_runners(request):
-  tnt_model_runner = base_runner.generate_tnt_model_runner(request.param)
-  reference_model_runner = base_runner.TrainingRunner(request.param.model_generator())
-  yield tnt_model_runner, reference_model_runner
-
 
 @pytest.fixture(scope="function")
 def setup_save_path(request):
@@ -42,6 +34,11 @@ def setup_save_path(request):
   if tnt.is_master_rank():
     shutil.rmtree(save_dir, ignore_errors=True)
 
+
+def gen_model_runners(model_config: base_runner.ModelConfig):
+  tnt_model_runner = base_runner.generate_tnt_model_runner(model_config)
+  reference_model_runner = base_runner.TrainingRunner(model_config.model_generator())
+  return tnt_model_runner, reference_model_runner
 
 def train_val_dataset_generator():
   micro_batch_size = 64
@@ -79,50 +76,57 @@ class CustomLearningRateScheduler(keras.callbacks.Callback):
     # Set the value back to the optimizer before this epoch starts
     tf.keras.backend.set_value(self.model.optimizer.lr, scheduled_lr)
 
-class TestsDataParallelCallbacks:
-  def train_tnt_and_ref_models_with_callbacks(self, callbacks, model_runners, number_epochs):
-    (train_dataset, val_dataset) = train_val_dataset_generator()
-    (ref_train_dataset, ref_val_dataset) = train_val_dataset_generator()
-  
-    tnt_model_runner, reference_model_runner = model_runners
 
-    param_dict = { 'epochs' : number_epochs,
-                   'verbose' : 0,
-                   'shuffle' : False,
-                   'callbacks' : callbacks }
-    tnt_history = tnt_model_runner.model.fit(train_dataset,
-                                             validation_data=val_dataset,
-                                             **param_dict)
-    ref_history = reference_model_runner.model.fit(ref_train_dataset,
-                                                   validation_data=ref_val_dataset,
-                                                   **param_dict)
-    return (tnt_history, ref_history)
-  
+def train_tnt_and_ref_models_with_callbacks(callbacks, model_config, number_epochs):
+  (train_dataset, val_dataset) = train_val_dataset_generator()
+  (ref_train_dataset, ref_val_dataset) = train_val_dataset_generator()
+
+  tnt_model_runner, reference_model_runner = gen_model_runners(model_config)
+
+  param_dict = { 'epochs' : number_epochs,
+                  'verbose' : 0,
+                  'shuffle' : False,
+                  'callbacks' : callbacks }
+  tnt_history = tnt_model_runner.model.fit(train_dataset,
+                                            validation_data=val_dataset,
+                                            **param_dict)
+  ref_history = reference_model_runner.model.fit(ref_train_dataset,
+                                                 validation_data=ref_val_dataset,
+                                                 **param_dict)
+  return (tnt_history, ref_history)
+
+
+@pytest.mark.parametrize("model_config", [base_runner.ModelConfig(mnist.fc_model_generator),
+                                          base_runner.ModelConfig(mnist.subclassed_model_generator),
+                                          base_runner.ModelConfig(mnist.fc_model_generator,
+                                                                  tnt.ParallelStrategy.PIPELINING),
+                                          ])
+class TestTarantellaCallbacks:
   @pytest.mark.parametrize("number_epochs", [5])
-  def test_learning_rate_scheduler_callback(self, model_runners, number_epochs):
+  def test_learning_rate_scheduler_callback(self, model_config, number_epochs):
     callbacks = [tf.keras.callbacks.LearningRateScheduler(schedule=(lambda epoch, lr: 0.1 * lr),
                                                           verbose=1)]
-    tnt_history, reference_history = self.train_tnt_and_ref_models_with_callbacks(
-                                       callbacks, model_runners, number_epochs)
+    tnt_history, reference_history = train_tnt_and_ref_models_with_callbacks(
+                                       callbacks, model_config, number_epochs)
 
     for key in reference_history.history.keys():
       assert all(np.isclose(tnt_history.history[key], reference_history.history[key], atol=1e-6))
   
   @pytest.mark.parametrize("number_epochs", [5])
-  def test_custom_callback(self, model_runners, number_epochs):
+  def test_custom_callback(self, model_config, number_epochs):
     callbacks = [CustomLearningRateScheduler()]
-    tnt_history, reference_history = self.train_tnt_and_ref_models_with_callbacks(
-                                       callbacks, model_runners, number_epochs)
+    tnt_history, reference_history = train_tnt_and_ref_models_with_callbacks(
+                                       callbacks, model_config, number_epochs)
 
     for key in reference_history.history.keys():
       assert all(np.isclose(tnt_history.history[key], reference_history.history[key], atol=1e-6))
 
   @pytest.mark.parametrize("number_epochs", [5])
-  def test_lambda_callback(self, model_runners, number_epochs):
+  def test_lambda_callback(self, model_config, number_epochs):
     (train_dataset, val_dataset) = train_val_dataset_generator()
     (ref_train_dataset, ref_val_dataset) = train_val_dataset_generator()
 
-    tnt_model_runner, reference_model_runner = model_runners
+    tnt_model_runner, reference_model_runner = gen_model_runners(model_config)
 
     lr_callback = CustomLearningRateScheduler(reference_model_runner.model)
     lr_lambda_callback = LambdaCallback(
@@ -152,9 +156,9 @@ class TestsDataParallelCallbacks:
       assert all(np.isclose(tnt_history.history[key], reference_history.history[key], atol=1e-6))
 
   @pytest.mark.parametrize("number_epochs", [1])
-  def test_tensorboard_callback(self, setup_save_path, model_runners, number_epochs):
+  def test_tensorboard_callback(self, setup_save_path, model_config, number_epochs):
     (train_dataset, val_dataset) = train_val_dataset_generator()
-    tnt_model_runner, _ = model_runners
+    tnt_model_runner, _ = gen_model_runners(model_config)
 
     tnt_model_runner.model.fit(train_dataset, validation_data=val_dataset,
                                epochs = number_epochs,
@@ -163,42 +167,34 @@ class TestsDataParallelCallbacks:
     assert os.path.isdir(os.path.join(setup_save_path, "validation"))
 
   @pytest.mark.parametrize("number_epochs", [1])
-  def test_model_checkpoint_callback(self, setup_save_path, model_runners, number_epochs):
-    checkpoint_path = os.path.join(setup_save_path, "logs")
-    callbacks = [tf.keras.callbacks.ModelCheckpoint(filepath = checkpoint_path)]
-    self.train_tnt_and_ref_models_with_callbacks(callbacks, model_runners, number_epochs)
-    # FIXME: assert correct file exists
-    assert True
-
-  @pytest.mark.parametrize("number_epochs", [1])
-  def test_history_callback(self, model_runners, number_epochs):
+  def test_history_callback(self, model_config, number_epochs):
     # history callback is added by default
     callbacks = []
-    tnt_history, reference_history = self.train_tnt_and_ref_models_with_callbacks(
-                                       callbacks, model_runners, number_epochs)
+    tnt_history, reference_history = train_tnt_and_ref_models_with_callbacks(
+                                       callbacks, model_config, number_epochs)
 
     for key in reference_history.history.keys():
       assert all(np.isclose(tnt_history.history[key], reference_history.history[key], atol=1e-6))
 
   @pytest.mark.parametrize("number_epochs", [10])
-  def test_early_stopping_callback(self, model_runners, number_epochs):
+  def test_early_stopping_callback(self, model_config, number_epochs):
     monitor_metric = 'val_loss'
     callbacks = [tf.keras.callbacks.EarlyStopping(monitor=monitor_metric,
                                                   min_delta=0.1,
                                                   patience=1)]
-    tnt_history, reference_history = self.train_tnt_and_ref_models_with_callbacks(
-                                       callbacks, model_runners, number_epochs)
+    tnt_history, reference_history = train_tnt_and_ref_models_with_callbacks(
+                                       callbacks, model_config, number_epochs)
 
     # Expect both models to run same number of epochs
     assert len(tnt_history.history[monitor_metric]) == len(reference_history.history[monitor_metric])
 
   @pytest.mark.parametrize("number_epochs", [2])
-  def test_csv_logger_callback(self, setup_save_path, model_runners, number_epochs):
+  def test_csv_logger_callback(self, setup_save_path, model_config, number_epochs):
     (train_dataset, val_dataset) = train_val_dataset_generator()
     (ref_train_dataset, ref_val_dataset) = train_val_dataset_generator()
 
     filename = os.path.join(setup_save_path, "training")
-    tnt_model_runner, reference_model_runner = model_runners
+    tnt_model_runner, reference_model_runner = gen_model_runners(model_config)
     param_dict = { 'epochs' : number_epochs,
                    'verbose' : 0,
                    'shuffle' : False }
@@ -222,30 +218,30 @@ class TestsDataParallelCallbacks:
 
   # FIXME: This does not seem to even trigger a `NaN`
   @pytest.mark.parametrize("number_epochs", [1])
-  def test_terminate_callback(self, model_runners, number_epochs):
+  def test_terminate_callback(self, model_config, number_epochs):
     callbacks = [tf.keras.callbacks.TerminateOnNaN()]
-    tnt_history, ref_history = self.train_tnt_and_ref_models_with_callbacks(
-                                       callbacks, model_runners, number_epochs)
+    tnt_history, ref_history = train_tnt_and_ref_models_with_callbacks(
+                                       callbacks, model_config, number_epochs)
 
     for key in ref_history.history.keys():
       assert all(np.isclose(tnt_history.history[key], ref_history.history[key], atol=1e-6))
 
   @pytest.mark.parametrize("number_epochs", [1])
-  def test_base_logger_callback(self, model_runners, number_epochs):
+  def test_base_logger_callback(self, model_config, number_epochs):
     callbacks = [tf.keras.callbacks.BaseLogger()]
     with pytest.raises(ValueError):
-      tnt_history, ref_history = self.train_tnt_and_ref_models_with_callbacks(
-                                       callbacks, model_runners, number_epochs)
+      tnt_history, ref_history = train_tnt_and_ref_models_with_callbacks(
+                                       callbacks, model_config, number_epochs)
 
   @pytest.mark.parametrize("number_epochs", [3])
-  def test_reduce_lr_on_plateau_callback(self, model_runners, number_epochs):
+  def test_reduce_lr_on_plateau_callback(self, model_config, number_epochs):
     monitor_metric = 'val_loss'
     callbacks = [tf.keras.callbacks.ReduceLROnPlateau(monitor=monitor_metric,
                                                       factor=0.01,
                                                       min_delta=0.01,
                                                       patience=1)]
-    tnt_history, reference_history = self.train_tnt_and_ref_models_with_callbacks(
-                                       callbacks, model_runners, number_epochs)
+    tnt_history, reference_history = train_tnt_and_ref_models_with_callbacks(
+                                       callbacks, model_config, number_epochs)
 
     for key in reference_history.history.keys():
       assert all(np.isclose(tnt_history.history[key], reference_history.history[key], atol=1e-6))
@@ -255,7 +251,7 @@ class TestsDataParallelCallbacks:
   @pytest.mark.parametrize("use_explicit_progbarlogger", [True, False])
   @pytest.mark.parametrize("verbose", [0, 1, 2])
   @pytest.mark.parametrize("exec_type", ['fit_with_validation', 'fit_without_validation'])
-  def test_progbar_logger_callback_train(self, model_runners, number_epochs,
+  def test_progbar_logger_callback_train(self, model_config, number_epochs,
                                              use_explicit_progbarlogger, verbose, exec_type, capsys):
     (train_dataset, test_dataset) = train_val_dataset_generator()
     (ref_train_dataset, ref_test_dataset) = train_val_dataset_generator()
@@ -266,7 +262,7 @@ class TestsDataParallelCallbacks:
     tnt_callbacks = [ tf.keras.callbacks.ProgbarLogger(count_mode = 'steps') ] if use_explicit_progbarlogger else []
     ref_callbacks = [ tf.keras.callbacks.ProgbarLogger(count_mode = 'steps') ] if use_explicit_progbarlogger else []
 
-    tnt_model_runner, ref_model_runner = model_runners
+    tnt_model_runner, ref_model_runner = gen_model_runners(model_config)
 
     tnt_model_runner.model.fit(train_dataset, validation_data = test_dataset,
                                 epochs = number_epochs, callbacks = tnt_callbacks,
@@ -292,7 +288,7 @@ class TestsDataParallelCallbacks:
   @pytest.mark.parametrize("verbose", [2])  # FIXME: verbose = 1 does not issue the same values as the reference model
                                             # (becuse it processes micro-batches instead of batches)
   @pytest.mark.parametrize("exec_type", ['evaluate', 'predict'])
-  def test_progbar_logger_callback_inference(self, model_runners, number_epochs,
+  def test_progbar_logger_callback_inference(self, model_config, number_epochs,
                                              use_explicit_progbarlogger, verbose, exec_type, capsys):
     (train_dataset, test_dataset) = train_val_dataset_generator()
     (ref_train_dataset, ref_test_dataset) = train_val_dataset_generator()
@@ -300,7 +296,7 @@ class TestsDataParallelCallbacks:
     tnt_callbacks = [ tf.keras.callbacks.ProgbarLogger(count_mode = 'steps') ] if use_explicit_progbarlogger else []
     ref_callbacks = [ tf.keras.callbacks.ProgbarLogger(count_mode = 'steps') ] if use_explicit_progbarlogger else []
 
-    tnt_model_runner, ref_model_runner = model_runners
+    tnt_model_runner, ref_model_runner = gen_model_runners(model_config)
 
     if exec_type == 'evaluate':
       tnt_model_runner.model.evaluate(test_dataset, callbacks = tnt_callbacks, verbose = verbose)
@@ -321,3 +317,25 @@ class TestsDataParallelCallbacks:
     else:
       assert tnt_captured.out == ""
       assert tnt_captured.err == ""
+
+
+
+class TestModelCheckpointCallback:
+  @pytest.mark.parametrize("model_config", [base_runner.ModelConfig(mnist.fc_model_generator),
+                                            base_runner.ModelConfig(mnist.subclassed_model_generator)])
+  @pytest.mark.parametrize("number_epochs", [1])
+  def test_model_checkpoint_data_par(self, setup_save_path, model_config, number_epochs):
+    checkpoint_path = os.path.join(setup_save_path, "logs")
+    callbacks = [tf.keras.callbacks.ModelCheckpoint(filepath = checkpoint_path)]
+    train_tnt_and_ref_models_with_callbacks(callbacks, model_config, number_epochs)
+    # FIXME: assert correct file exists
+    assert True
+
+  @pytest.mark.parametrize("model_config", [base_runner.ModelConfig(mnist.fc_model_generator, tnt.ParallelStrategy.ALL)])
+  @pytest.mark.parametrize("number_epochs", [1])
+  def test_model_checkpoint_pipelining(self, setup_save_path, model_config, number_epochs):
+    checkpoint_path = os.path.join(setup_save_path, "logs")
+    callbacks = [tf.keras.callbacks.ModelCheckpoint(filepath = checkpoint_path)]
+    with pytest.raises(ValueError):
+      train_tnt_and_ref_models_with_callbacks(callbacks, model_config, number_epochs)
+
