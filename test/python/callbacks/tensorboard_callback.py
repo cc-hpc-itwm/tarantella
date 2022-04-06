57f29e7775c4d3a1ae0b5f77cdc54b51cd6f31e7
Alexandra <carpenamarie@itwm.fraunhofer.de>
2022-04-06 19:56:50 +0200

    tests: pytest: callbacks: Tensorboard: add distinct tests for single rank/multi rank callback calls

--- test/python/callbacks/tensorboard_callback.py
+++ test/python/callbacks/tensorboard_callback.py
@@ -6,30 +6,64 @@ import tarantella as tnt
 
 import tensorflow as tf
 
+import enum
 import os
 import pytest
 
 setup_save_path = callback_utilities.setup_save_path
 
-@pytest.mark.parametrize("model_config", [base_runner.ModelConfig(mnist.fc_model_generator),
-                                          base_runner.ModelConfig(mnist.subclassed_model_generator),
-                                          pytest.param(base_runner.ModelConfig(mnist.fc_model_generator,
+class CallbackWrapper(enum.Enum):
+  NONE = 0,
+  DEFAULT = 1,
+  ONE_RANK = 2,
+  MULTI_RANK = 3
+
+@pytest.mark.parametrize("model_config", [base_runner.ModelConfig(mnist.subclassed_model_generator),
+                                          pytest.param(base_runner.ModelConfig(mnist.fc_model_generator_four_partitions,
                                                                                tnt.ParallelStrategy.PIPELINING),
-                                                       marks=pytest.mark.skipif(tnt.get_size() != 1,
-                                                                                reason="Cannot run multi-rank, model has only one partition")),
+                                                       marks=pytest.mark.skipif(tnt.get_size() != 4,
+                                                                                reason="Model has 4 partitions, needs 4 ranks to run on")),
                                           ])
 class TestTensorboardCallbacks:
   @pytest.mark.parametrize("number_epochs", [1])
-  def test_tensorboard_callback(self, setup_save_path, model_config, number_epochs):
+  @pytest.mark.parametrize("wrapper_type", [CallbackWrapper.NONE,
+                                            CallbackWrapper.DEFAULT,
+                                            CallbackWrapper.ONE_RANK])
+  def test_tensorboard_one_rank(self, setup_save_path, model_config, number_epochs, wrapper_type):
     (train_dataset, val_dataset) = callback_utilities.train_val_dataset_generator()
     tnt_model_runner, _ = callback_utilities.gen_model_runners(model_config)
 
+    callback = tf.keras.callbacks.TensorBoard(log_dir = setup_save_path)
+    if wrapper_type == CallbackWrapper.DEFAULT:
+      callback = tnt.keras.callbacks.Callback(callback)
+    elif wrapper_type == CallbackWrapper.ONE_RANK:
+      callback = tnt.keras.callbacks.Callback(callback, run_on_all_ranks = False)
+
     tnt_model_runner.model.fit(train_dataset, validation_data=val_dataset,
                                epochs = number_epochs,
-                               callbacks = [tf.keras.callbacks.TensorBoard(log_dir = setup_save_path)])
+                               callbacks = [callback])
     result = [True]
     if tnt.is_master_rank():
       result = [os.path.isdir(os.path.join(setup_save_path, "train")),
                 os.path.isdir(os.path.join(setup_save_path, "validation"))]
       result = [all(result)]
+    util.assert_on_all_ranks(result)
+
+
+  @pytest.mark.parametrize("number_epochs", [1])
+  @pytest.mark.parametrize("wrapper_type", [CallbackWrapper.MULTI_RANK])
+  def test_tensorboard_multi_rank(self, setup_save_path, model_config, number_epochs, wrapper_type):
+    (train_dataset, val_dataset) = callback_utilities.train_val_dataset_generator()
+    tnt_model_runner, _ = callback_utilities.gen_model_runners(model_config)
+
+    callback = tf.keras.callbacks.TensorBoard(log_dir = setup_save_path)
+    if wrapper_type == CallbackWrapper.MULTI_RANK:
+      callback = tnt.keras.callbacks.Callback(callback, run_on_all_ranks = True)
+
+    tnt_model_runner.model.fit(train_dataset, validation_data=val_dataset,
+                               epochs = number_epochs,
+                               callbacks = [callback])
+    result = [os.path.isdir(os.path.join(setup_save_path, f"rank_{tnt.get_rank()}/train")),
+              os.path.isdir(os.path.join(setup_save_path, f"rank_{tnt.get_rank()}/validation"))]
+    result = [all(result)]
     util.assert_on_all_ranks(result)
