ec3b2afee9135eb8d190d4d839608a27420cb04d
Alexandra <carpenamarie@itwm.fraunhofer.de>
2022-04-12 15:54:55 +0200

    tests: pytest: ModelCheckpoint: update to check for created directories

--- test/python/callbacks/modelcheckpoint_callback.py
+++ test/python/callbacks/modelcheckpoint_callback.py
@@ -1,18 +1,11 @@
-# CustomLearningRateScheduler:
-#   Copyright (C) 2021 keras.io <https://www.tensorflow.org/guide/keras/custom_callback#learning_rate_scheduling>
-#   Modifications Copyright (C) 2022 Fraunhofer ITWM <http://www.itwm.fraunhofer.de/>
-
 from models import mnist_models as mnist
-import training_runner as base_runner
 import callback_utilities
+import training_runner as base_runner
 import utilities as util
+
 import tarantella as tnt
 
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.callbacks import LambdaCallback
-
-import numpy as np
 import os
 import pytest
 
@@ -20,18 +13,30 @@ setup_save_path = callback_utilities.setup_save_path
 
 class TestModelCheckpointCallback:
   @pytest.mark.parametrize("model_config", [base_runner.ModelConfig(mnist.fc_model_generator)])
-  @pytest.mark.parametrize("number_epochs", [1])
-  def test_model_checkpoint_data_par(self, setup_save_path, model_config, number_epochs):
+  def test_model_checkpoint_data_par(self, setup_save_path, model_config):
+    train_dataset, val_dataset = callback_utilities.train_val_dataset_generator()
+    tnt_model_runner, _ = callback_utilities.gen_model_runners(model_config)
+
     callbacks = [tf.keras.callbacks.ModelCheckpoint(filepath = setup_save_path)]
-    callback_utilities.train_tnt_and_ref_models_with_callbacks(callbacks, model_config, number_epochs)
-    # FIXME: assert correct file exists
-    assert True
+    tnt_model_runner.model.fit(train_dataset, validation_data=val_dataset, epochs = 1,
+                               callbacks = callbacks)
+    # only the master rank should save the model
+    # the other ranks may use the same shared directory or a local path as their `filepath`
+    result = [True]
+    if tnt.is_master_rank():
+      result = [os.path.isdir(os.path.join(setup_save_path, "assets")),
+                os.path.isdir(os.path.join(setup_save_path, "variables"))]
+      result = [all(result)]
+    util.assert_on_all_ranks(result)
 
   @pytest.mark.parametrize("model_config", [base_runner.ModelConfig(mnist.fc_model_generator,
                                                                     tnt.ParallelStrategy.ALL)])
-  @pytest.mark.parametrize("number_epochs", [1])
-  def test_model_checkpoint_pipelining(self, setup_save_path, model_config, number_epochs):
+  def test_model_checkpoint_pipelining(self, setup_save_path, model_config):
+    train_dataset, _ = callback_utilities.train_val_dataset_generator()
+    tnt_model_runner, _ = callback_utilities.gen_model_runners(model_config)
+
     callbacks = [tf.keras.callbacks.ModelCheckpoint(filepath = setup_save_path)]
     with pytest.raises(ValueError):
-      callback_utilities.train_tnt_and_ref_models_with_callbacks(callbacks, model_config, number_epochs)
+      tnt_model_runner.model.fit(train_dataset, epochs = 1,
+                                 callbacks = callbacks)
 
